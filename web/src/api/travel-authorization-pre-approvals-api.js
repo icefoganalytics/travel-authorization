10c54f4193f8c625f88b20c8bf6c66f578bdcd6f
Marlen Brunner <klondikemarlen@gmail.com>
2025-03-20 09:55:38 -0700

    :sparkles: Update travel authorization pre-approvals front-end api to match new back-end.
    Add JSDoc types.

--- web/src/api/travel-authorization-pre-approvals-api.js
+++ web/src/api/travel-authorization-pre-approvals-api.js
@@ -1,5 +1,8 @@
 import http from "@/api/http-client"
 
+/** @typedef {import('@/api/base-api.js').Policy} Policy */
+/** @typedef {import("@/api/base-api").ModelOrder} ModelOrder */
+
 /** Keep in sync with api/src/models/travel-authorization-pre-approval.ts */
 export const TRAVEL_AUTHORIZATION_PRE_APPROVAL_STATUSES = Object.freeze({
   DRAFT: "draft",
@@ -8,12 +11,127 @@ export const TRAVEL_AUTHORIZATION_PRE_APPROVAL_STATUSES = Object.freeze({
   DECLINED: "declined",
 })
 
+/** @typedef {TRAVEL_AUTHORIZATION_PRE_APPROVAL_STATUSES[keyof TRAVEL_AUTHORIZATION_PRE_APPROVAL_STATUSES]} TravelAuthorizationPreApprovalStatuses */
+
+/**
+ * Keep in sync with api/src/models/travel-authorization-pre-approval.ts
+ * @typedef {{
+ *   id: number,
+ *   estimatedCost: number,
+ *   location: string,
+ *   department: string | null,
+ *   branch: string | null,
+ *   purpose: string | null,
+ *   reason: string | null,
+ *   startDate: string | null,
+ *   endDate: string | null,
+ *   isOpenForAnyDate: boolean,
+ *   month: string | null,
+ *   isOpenForAnyTraveler: boolean,
+ *   numberTravelers: number | null,
+ *   travelerNotes: string | null,
+ *   status: TravelAuthorizationPreApprovalStatuses,
+ *   createdAt: string,
+ *   updatedAt: string,
+ * }} TravelAuthorizationPreApproval
+ */
+
+/**
+ * @typedef {{
+ *   id?: number,
+ *   location?: string,
+ *   department?: string | null,
+ *   branch?: string | null,
+ *   startDate?: string | null,
+ *   endDate?: string | null,
+ *   isOpenForAnyDate?: boolean,
+ *   month?: string | null,
+ *   isOpenForAnyTraveler?: boolean,
+ *   numberTravelers?: number | null,
+ *   status?: TravelAuthorizationPreApprovalStatuses,
+ * }} TravelAuthorizationPreApprovalWhereOptions
+ */
+
+/**
+ * // match with model scopes signatures
+ * @typedef {{}} TravelAuthorizationPreApprovalFiltersOptions
+ */
+
+/**
+ * @typedef {{
+ *   where?: TravelAuthorizationPreApprovalWhereOptions,
+ *   filters?: TravelAuthorizationPreApprovalFiltersOptions,
+ *   order?: ModelOrder[],
+ *   page?: number,
+ *   perPage?: number,
+ * }} TravelAuthorizationPreApprovalsQueryOptions
+ */
+
 export const travelAuthorizationPreApprovalsApi = {
   STATUSES: TRAVEL_AUTHORIZATION_PRE_APPROVAL_STATUSES,
-  async list({ where, ...otherParams } = {}) {
+
+  /**
+   * @param {TravelAuthorizationPreApprovalsQueryOptions} [params={}]
+   * @returns {Promise<{
+   *   travelAuthorizationPreApprovals: TravelAuthorizationPreApproval[],
+   *   totalCount: number,
+   * }>}
+   */
+  async list(params = {}) {
     const { data } = await http.get("/api/travel-authorization-pre-approvals", {
-      params: { where, ...otherParams },
+      params,
     })
+    return data
+  },
+
+  /**
+   * @param {number} travelAuthorizationPreApprovalId
+   * @returns {Promise<{
+   *   travelAuthorizationPreApproval: TravelAuthorizationPreApproval,
+   *   policy: Policy,
+   * }>
+   */
+  async get(travelAuthorizationPreApprovalId) {
+    const { data } = await http.get(
+      `/api/travel-authorization-pre-approvals/${travelAuthorizationPreApprovalId}`
+    )
+    return data
+  },
+
+  /**
+   * @param {Partial<TravelAuthorizationPreApproval>} attributes
+   * @returns {Promise<{
+   *   travelAuthorizationPreApproval: TravelAuthorizationPreApproval,
+   * }>}
+   */
+  async create(attributes) {
+    const { data } = await http.post("/api/travel-authorization-pre-approvals", attributes)
+    return data
+  },
+
+  /**
+   * @param {number} travelAuthorizationPreApprovalId
+   * @param {Partial<TravelAuthorizationPreApproval>} attributes
+   * @returns {Promise<{
+   *   travelAuthorizationPreApproval: TravelAuthorizationPreApproval,
+   * }>}
+   */
+  async update(travelAuthorizationPreApprovalId, attributes) {
+    const { data } = await http.patch(
+      `/api/travel-authorization-pre-approvals/${travelAuthorizationPreApprovalId}`,
+      attributes
+    )
+    return data
+  },
+
+  /**
+   * @param {number} travelAuthorizationPreApprovalId
+   * @returns {Promise<void>}
+   */
+  async delete(travelAuthorizationPreApprovalId) {
+    const { data } = await http.delete(
+      `/api/travel-authorization-pre-approvals/${travelAuthorizationPreApprovalId}`
+    )
     return data
   },
 }
