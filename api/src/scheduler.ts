186b0f029eb4dbd678216ddcd5f7659d503e945e
Marlen Brunner <klondikemarlen@gmail.com>
2025-03-24 16:53:02 -0700

    :hourglass: Schedule YgEmployeeGroups sync.
    Also fix job definition for YgEmployees sync.
    cronTime: "* 0 6 * * *", means run job once a second for the minute of 6:00am.

--- api/src/scheduler.ts
+++ api/src/scheduler.ts
@@ -1,7 +1,7 @@
 import { CronJob } from "cron"
 
 import { logger } from "@/utils/logger"
-import { YgEmployees } from "@/services"
+import { YgEmployees, YgEmployeeGroups } from "@/services"
 
 logger.debug(`Scheduler started in app container.`)
 
@@ -9,12 +9,32 @@ logger.debug(`Scheduler started in app container.`)
  * See https://www.npmjs.com/package/cron.
  *
  * Most useful debugging option is `runOnInit: true`, which will immediately executes the job.
+ *
+ * Allowed fields
+ * # ┌────────────── second (optional)
+ * # │ ┌──────────── minute
+ * # │ │ ┌────────── hour
+ * # │ │ │ ┌──────── day of month
+ * # │ │ │ │ ┌────── month
+ * # │ │ │ │ │ ┌──── day of week
+ * # │ │ │ │ │ │
+ * # │ │ │ │ │ │
+ * # * * * * * *
  */
 
 CronJob.from({
-  cronTime: "* 0 6 * * *",
+  cronTime: "0 0 6 * * *",
   async onTick() {
     await YgEmployees.SyncService.perform()
+  },
+  start: true,
+  timeZone: "UTC-7",
+})
+
+CronJob.from({
+  cronTime: "0 10 6 * * *",
+  async onTick() {
+    await YgEmployeeGroups.SyncService.perform()
   },
   start: true,
   timeZone: "UTC-7",
