442c82534c0fdc59fff60a56116853cb601c7cb8
Marlen Brunner <klondikemarlen@gmail.com>
2025-03-25 15:22:08 -0700

    :construction: Rework travel attribute creation some more.
    There is a lot of overlap between pre-approval attributes and pre-approval profile atttributes.
    The data model might need some denormalization?

---
+++ web/src/components/travel-authorization-pre-approvals/TravelAuthorizationPreApprovalTravelerAttributesFormCard.vue
@@ -0,0 +1,260 @@
+<template>
+  <HeaderActionsFormCard
+    title="Traveller Details"
+    header-tag="h3"
+  >
+    <v-row>
+      <v-col
+        cols="12"
+        md="12"
+      >
+        <v-switch
+          :input-value="exactTravelerKnown"
+          :label="exactTravelerKnown ? 'Exact traveler known' : 'Exact traveler not known'"
+          inset
+          @change="toggleExactTravelerKnown"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <DepartmentAutocomplete
+          v-model="department"
+          label="Department *"
+          :rules="[required]"
+          outlined
+          @input="resetDependentFieldsDepartment"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <BranchAutocomplete
+          v-model="branch"
+          label="Branch"
+          :disabled="isNil(department)"
+          :where="branchWhere"
+          outlined
+          clearable
+          @input="resetDependentFieldsBranch"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row v-if="exactTravelerKnown">
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <YgEmployeeAutocomplete
+          v-model="travelerName"
+          item-value="fullName"
+          item-text="fullName"
+          label="Traveler name *"
+          hint="Search for a traveler"
+          :clearable="false"
+          outlined
+          :where="ygEmployeeWhere"
+          :rules="[required]"
+        />
+      </v-col>
+    </v-row>
+    <v-row v-else>
+      <v-col
+        cols="12"
+        md="4"
+      >
+        <v-text-field
+          v-model="numberTravelers"
+          label="Number of Travelers *"
+          type="number"
+          outlined
+          :rules="[required]"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col
+        cols="12"
+        md="3"
+      >
+        <v-btn
+          color="primary"
+          @click="addTravelerProfileAttributes"
+        >
+          Add traveler profile
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col
+        cols="12"
+        md="9"
+      >
+        <v-data-table
+          :headers="headers"
+          :items="profilesAttributesReadOnly"
+          hide-default-footer
+        >
+          <template #item.actions="{ item }">
+            <v-btn
+              title="Remove traveler profile"
+              icon
+              color="error"
+              @click="removeTravelerProfileAttributes(item)"
+            >
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </HeaderActionsFormCard>
+</template>
+
+<script setup>
+import { computed, ref } from "vue"
+import { isEqual, isNil } from "lodash"
+
+import { required } from "@/utils/validators"
+
+import HeaderActionsFormCard from "@/components/common/HeaderActionsFormCard.vue"
+import YgEmployeeAutocomplete from "@/components/yg-employees/YgEmployeeAutocomplete.vue"
+import DepartmentAutocomplete from "@/components/yg-employee-groups/DepartmentAutocomplete.vue"
+import BranchAutocomplete from "@/components/yg-employee-groups/BranchAutocomplete.vue"
+
+/** @typedef {import("@/api/travel-authorization-pre-approvals-api").TravelAuthorizationPreApproval} TravelAuthorizationPreApproval */
+/** @typedef {import("@/api/travel-authorization-pre-approvals-api").TravelAuthorizationPreApprovalProfile} TravelAuthorizationPreApprovalProfile */
+
+const props = defineProps({
+  value: {
+    /**
+     * @type {Partial<TravelAuthorizationPreApproval> & {
+     *   profilesAttributes: Partial<TravelAuthorizationPreApprovalProfile>[]
+     * }}
+     */
+    type: Object,
+    default: () => ({
+      profilesAttributes: [],
+    }),
+  },
+})
+
+const emit = defineEmits(["input"])
+
+const profilesAttributesReadOnly = computed(() => props.value.profilesAttributes)
+
+const exactTravelerKnown = ref(true)
+const department = ref(undefined)
+const branch = ref(undefined)
+const travelerName = ref(undefined)
+const numberTravelers = ref(undefined)
+
+const branchWhere = computed(() => ({
+  department: department.value,
+}))
+
+const ygEmployeeWhere = computed(() => ({
+  department: department.value,
+  branch: branch.value,
+}))
+
+function resetDependentFieldsDepartment() {
+  branch.value = undefined
+  travelerName.value = undefined
+  numberTravelers.value = undefined
+}
+
+function resetDependentFieldsBranch() {
+  travelerName.value = undefined
+  numberTravelers.value = undefined
+}
+
+const headers = ref([
+  {
+    text: "Name",
+    value: "profileName",
+  },
+  {
+    text: "Dept.",
+    value: "department",
+  },
+  {
+    text: "Branch",
+    value: "branch",
+  },
+  {
+    text: "Actions",
+    value: "actions",
+    sortable: false,
+  },
+])
+
+function toggleExactTravelerKnown(value) {
+  exactTravelerKnown.value = value
+  department.value = undefined
+  branch.value = undefined
+  travelerName.value = undefined
+
+  const { profilesAttributes, ...otherAttributes } = props.value
+  const newPreApprovalAttributes = {
+    ...otherAttributes,
+    isOpenForAnyTraveler: !exactTravelerKnown.value,
+    department: undefined,
+    branch: undefined,
+    numberTravelers: undefined,
+    profilesAttributes: [],
+  }
+  emit("input", newPreApprovalAttributes)
+}
+
+function addTravelerProfileAttributes() {
+  const { profilesAttributes, ...otherAttributes } = props.value
+  if (exactTravelerKnown.value) {
+    const newProfileAttributes = {
+      profileName: travelerName.value,
+      department: department.value,
+      branch: branch.value,
+    }
+    const newPreApprovalAttributes = {
+      ...otherAttributes,
+      profilesAttributes: [...profilesAttributes, newProfileAttributes],
+    }
+    emit("input", newPreApprovalAttributes)
+    return
+  } else {
+    const profileName = [department.value, branch.value].filter(Boolean).join(" ") + " staff"
+    const newProfileAttributes = {
+      profileName,
+      department: department.value,
+      branch: branch.value,
+    }
+    const newPreApprovalAttributes = {
+      ...otherAttributes,
+      profilesAttributes: [...profilesAttributes, newProfileAttributes],
+    }
+
+    emit("input", newPreApprovalAttributes)
+    return
+  }
+}
+
+function removeTravelerProfileAttributes(item) {
+  const { profilesAttributes, ...otherAttributes } = props.value
+  const travelerProfilesAttributesWithoutItem = profilesAttributes.filter(
+    (profile) => !isEqual(profile, item)
+  )
+  const newPreApprovalAttributes = {
+    ...otherAttributes,
+    profilesAttributes: travelerProfilesAttributesWithoutItem,
+  }
+  emit("input", newPreApprovalAttributes)
+}
+</script>
--- web/src/pages/travel-pre-approvals/TravelPreApprovalNewPage.vue
+++ web/src/pages/travel-pre-approvals/TravelPreApprovalNewPage.vue
@@ -4,7 +4,6 @@
     title="New Travel Pre-Approval"
     header-tag="h2"
     lazy-validation
-    :loading="isLoading"
     @submit.prevent="createTravelAuthorizationPreApproval"
   >
     <v-row>
@@ -126,171 +125,23 @@
 
     <v-row>
       <v-col>
-        <v-card>
-          <v-card-title>
-            <h3 class="text-h6 mb-0">Traveller Details</h3>
-          </v-card-title>
-          <v-divider />
-          <v-card-text>
-            <v-row>
-              <v-col
-                cols="12"
-                md="12"
-              >
-                <v-switch
-                  :input-value="exactTravelerKnown"
-                  :label="exactTravelerKnown ? 'Exact traveler known' : 'Exact traveler not known'"
-                  inset
-                  @change="toggleExactTravelerKnown"
-                />
-              </v-col>
-            </v-row>
+        <TravelAuthorizationPreApprovalTravelerAttributesFormCard
+          v-model="travelAuthorizationPreApprovalAttributes"
+        />
+      </v-col>
+    </v-row>
 
-            <template v-if="travelAuthorizationPreApprovalAttributes.isOpenForAnyTraveler">
-              <v-row>
-                <v-col
-                  cols="12"
-                  md="6"
-                >
-                  <DepartmentAutocomplete
-                    v-model="travelAuthorizationPreApprovalAttributes.department"
-                    label="Department *"
-                    :rules="[required]"
-                    outlined
-                  />
-                </v-col>
-                <v-col
-                  cols="12"
-                  md="6"
-                >
-                  <BranchAutocomplete
-                    v-model="travelAuthorizationPreApprovalAttributes.branch"
-                    label="Branch"
-                    :disabled="isNil(travelAuthorizationPreApprovalAttributes.department)"
-                    :where="branchWhere"
-                    outlined
-                  />
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <v-text-field
-                    v-model="travelAuthorizationPreApprovalAttributes.numberTravelers"
-                    label="Number of Travelers *"
-                    type="number"
-                    outlined
-                    :rules="[required]"
-                  />
-                </v-col>
-              </v-row>
-            </template>
-            <template v-else>
-              <v-row>
-                <v-col
-                  cols="12"
-                  md="6"
-                >
-                  <DepartmentAutocomplete
-                    v-model="travelAuthorizationPreApprovalAttributes.department"
-                    label="Department"
-                    hint="Filter traveler name by department (optional)"
-                    outlined
-                    clearable
-                  />
-                </v-col>
-                <v-col
-                  cols="12"
-                  md="6"
-                >
-                  <BranchAutocomplete
-                    v-model="travelAuthorizationPreApprovalAttributes.branch"
-                    label="Branch"
-                    hint="Filter traveler name by branch (optional)"
-                    :disabled="isNil(travelAuthorizationPreApprovalAttributes.department)"
-                    :where="branchWhere"
-                    outlined
-                    clearable
-                  />
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col
-                  cols="12"
-                  md="4"
-                >
-                  <YgEmployeeAutocomplete
-                    v-model="travelerName"
-                    item-value="fullName"
-                    item-text="fullName"
-                    label="Traveler name *"
-                    hint="Search for a traveler"
-                    :clearable="false"
-                    outlined
-                    :where="ygEmployeeWhere"
-                    :rules="[required]"
-                  />
-                </v-col>
-              </v-row>
-            </template>
-            <v-row>
-              <v-col
-                cols="12"
-                md="3"
-              >
-                <!-- TODO: trigger traveller profile creation -->
-                <v-btn color="primary"> Add traveler profile(s) </v-btn>
-              </v-col>
-            </v-row>
-
-            <v-row>
-              <v-col
-                cols="12"
-                md="9"
-              >
-                <v-data-table
-                  :headers="headers"
-                  :items="profiles"
-                  hide-default-footer
-                >
-                  <template #item.actions="{ item }">
-                    <v-btn
-                      v-if="!readonly"
-                      style="min-width: 0"
-                      color="transparent"
-                      class="px-1"
-                      small
-                      @click="removeTraveller(item)"
-                    >
-                      <v-icon
-                        class=""
-                        color="red"
-                        >mdi-account-remove</v-icon
-                      >
-                    </v-btn>
-                  </template>
-                </v-data-table>
-              </v-col>
-            </v-row>
-
-            <v-row>
-              <v-col
-                cols="12"
-                md="12"
-              >
-                <v-textarea
-                  v-model="travellerNotes"
-                  :readonly="readonly"
-                  label="Traveller Notes"
-                  outlined
-                  :clearable="!readonly"
-                />
-              </v-col>
-            </v-row>
-          </v-card-text>
-        </v-card>
+    <v-row>
+      <v-col
+        cols="12"
+        md="12"
+      >
+        <v-textarea
+          v-model="travelAuthorizationPreApprovalAttributes.travelerNotes"
+          label="Traveler Notes"
+          outlined
+          clearable
+        />
       </v-col>
     </v-row>
 
@@ -318,26 +169,17 @@
 </template>
 
 <script setup>
-import { computed, ref, nextTick } from "vue"
-import { useStore } from "vue2-helpers/vuex"
-import { isNil } from "lodash"
+import { ref } from "vue"
 
 import { required } from "@/utils/validators"
-import { PREAPPROVED_URL } from "@/urls"
-import http from "@/api/http-client"
-import { TRAVEL_AUTHORIZATION_PRE_APPROVAL_STATUSES } from "@/api/travel-authorization-pre-approvals-api"
-import { TRAVEL_AUTHORIZATION_PRE_APPROVAL_SUBMISSION_STATUSES } from "@/api/travel-authorization-pre-approval-submissions-api"
 
 import useBreadcrumbs from "@/use/use-breadcrumbs"
-import useCurrentUser from "@/use/use-current-user"
 
 import HeaderActionsFormCard from "@/components/common/HeaderActionsFormCard.vue"
 import LocationsAutocomplete from "@/components/locations/LocationsAutocomplete.vue"
 import MonthSelect from "@/components/common/MonthSelect.vue"
 import TravelPurposeSelect from "@/components/travel-purposes/TravelPurposeSelect.vue"
-import YgEmployeeAutocomplete from "@/components/yg-employees/YgEmployeeAutocomplete.vue"
-import DepartmentAutocomplete from "@/components/yg-employee-groups/DepartmentAutocomplete.vue"
-import BranchAutocomplete from "@/components/yg-employee-groups/BranchAutocomplete.vue"
+import TravelAuthorizationPreApprovalTravelerAttributesFormCard from "@/components/travel-authorization-pre-approvals/TravelAuthorizationPreApprovalTravelerAttributesFormCard.vue"
 
 /** @typedef {import('@/api/travel-authorization-pre-approvals-api').TravelAuthorizationPreApproval} TravelAuthorizationPreApproval */
 
@@ -351,8 +193,6 @@ const props = defineProps({
     default: () => ({}),
   },
 })
-
-const { isAdmin } = useCurrentUser()
 
 /** @type {Partial<TravelAuthorizationPreApproval>} */
 const travelAuthorizationPreApprovalAttributes = ref({
@@ -369,119 +209,8 @@ const travelAuthorizationPreApprovalAttributes = ref({
   isOpenForAnyTraveler: undefined,
   numberTravelers: undefined,
   travelerNotes: undefined,
+  profilesAttributes: [],
 })
-
-const exactTravelerKnown = ref(true)
-
-function toggleExactTravelerKnown(value) {
-  exactTravelerKnown.value = value
-  if (value) {
-    travelAuthorizationPreApprovalAttributes.value.isOpenForAnyTraveler = false
-    travelAuthorizationPreApprovalAttributes.value.department = undefined
-    travelAuthorizationPreApprovalAttributes.value.branch = undefined
-    travelAuthorizationPreApprovalAttributes.value.numberTravelers = undefined
-    travelerName.value = undefined
-  } else {
-    travelAuthorizationPreApprovalAttributes.value.isOpenForAnyTraveler = true
-    travelAuthorizationPreApprovalAttributes.value.department = undefined
-    travelAuthorizationPreApprovalAttributes.value.branch = undefined
-    travelAuthorizationPreApprovalAttributes.value.numberTravelers = undefined
-    travelerName.value = undefined
-  }
-}
-
-const branchWhere = computed(() => ({
-  department: travelAuthorizationPreApprovalAttributes.value.department,
-}))
-
-const travelerName = ref(undefined)
-
-const ygEmployeeWhere = computed(() => ({
-  department: travelAuthorizationPreApprovalAttributes.value.department,
-  branch: travelAuthorizationPreApprovalAttributes.value.branch,
-}))
-
-const headers = ref([
-  {
-    text: "Name",
-    value: "profileName",
-  },
-  {
-    text: "Dept.",
-    value: "department",
-  },
-  {
-    text: "Branch",
-    value: "branch",
-  },
-  {
-    text: "Actions",
-    value: "actions",
-    sortable: false,
-  },
-])
-
-const profiles = ref([])
-const addNewTravelDialog = ref(false)
-const unknownDate = ref(false)
-const cost = ref("")
-const reason = ref("")
-const startDate = ref("")
-const endDate = ref("")
-const lockDepartment = ref(false)
-const department = ref("")
-const departmentList = ref([])
-const branch = ref("")
-const branchList = ref([])
-const undefinedTraveller = ref(false)
-const undefinedTravellerHint = ref("")
-const profilesNum = ref(null)
-const anticipatedMonth = ref("")
-const travellerNotes = ref("")
-
-const travellerDialog = ref(false)
-const employeeList = ref([])
-const adNameList = ref([])
-const adName = ref("")
-const isLoading = ref(false)
-const loadingData = ref(false)
-const showApproval = ref(false)
-const approved = ref(false)
-const approvedBy = ref("")
-const approvalDate = ref("")
-const readonly = ref(false)
-const deleteDialog = ref(false)
-const state = ref({
-  costErr: false,
-  departmentErr: false,
-  branchErr: false,
-  anticipatedMonthErr: false,
-  travellerNumErr: false,
-  startDateErr: false,
-  endDateErr: false,
-  unknownDateErr: false,
-  undefinedTravellerErr: false,
-})
-const adNameErr = ref(false)
-
-function addTraveller() {
-  if (adName.value) {
-    travellerDialog.value = false
-    const profileIndex = profiles.value.findIndex(
-      (profile) => profile.profileName === adName.value && profile.department === department.value
-    )
-    if (profileIndex < 0)
-      profiles.value.push({
-        profileName: adName.value,
-        department: department.value,
-        branch: branch.value,
-      })
-  } else adNameErr.value = true
-}
-
-function showTravellerProfileCreateDialog() {
-  travellerDialog.value = true
-}
 
 function clearDateRelatedFields() {
   travelAuthorizationPreApprovalAttributes.value.startDate = undefined
@@ -489,194 +218,7 @@ function clearDateRelatedFields() {
   travelAuthorizationPreApprovalAttributes.value.month = undefined
 }
 
-async function selectUndefinedTraveller() {
-  undefinedTravellerHint.value = ""
-  state.value.departmentErr = department.value ? false : true
-  state.value.branchErr = branchList.value.length > 0 && !branch.value ? true : false
-  if (!undefinedTraveller.value) {
-    profiles.value = []
-    return
-  }
-  if (department.value && (branch.value || branchList.value.length == 0)) {
-    state.value.undefinedTravellerErr = false
-    addUndefinedTraveller()
-  } else {
-    await nextTick()
-    undefinedTraveller.value = false
-    undefinedTravellerHint.value = "Please Select the Department and Branch First!"
-  }
-}
-
-function addUndefinedTraveller() {
-  state.value.travellerNumErr = false
-  if (profiles.value.length > 0) {
-    profiles.value = [
-      {
-        profileName: department.value + " " + (branch.value ? branch.value + " " : "") + "staff",
-        department: department.value,
-        branch: branch.value,
-      },
-    ]
-  }
-}
-
-function checkFields() {
-  state.value.costErr = cost.value ? false : true
-
-  state.value.unknownDateErr =
-    !startDate.value && !endDate.value && !unknownDate.value ? true : false
-  state.value.anticipatedMonthErr = unknownDate.value && !anticipatedMonth.value ? true : false
-
-  state.value.startDateErr = !startDate.value && endDate.value && !unknownDate.value ? true : false
-  state.value.endDateErr = startDate.value && !endDate.value && !unknownDate.value ? true : false
-
-  state.value.undefinedTravellerErr =
-    !undefinedTraveller.value && profiles.value.length == 0 ? true : false
-  state.value.travellerNumErr =
-    undefinedTraveller.value && (!profilesNum.value || profilesNum.value < 1) ? true : false
-
-  for (const key of Object.keys(state.value)) {
-    if (state.value[key]) return false
-  }
-  return true
-}
-
-async function createTravelAuthorizationPreApproval() {
-  if (checkFields()) {
-    isLoading.value = true
-    const body = {
-      location: travelAuthorizationPreApprovalAttributes.value.location,
-      purpose: travelAuthorizationPreApprovalAttributes.value.purpose,
-      estimatedCost: cost.value,
-      reason: reason.value,
-      isOpenForAnyDate: unknownDate.value,
-      month: anticipatedMonth.value,
-      startDate: !unknownDate.value ? startDate.value : null,
-      endDate: !unknownDate.value ? endDate.value : null,
-      department: department.value,
-      branch: branch.value,
-      isOpenForAnyTraveler: undefinedTraveller.value,
-      numberTravelers: profilesNum.value,
-      profiles: profiles.value,
-      travelerNotes: travellerNotes.value,
-    }
-    const id = props.travelRequest.value?.id ? props.travelRequest.value.id : 0
-    return http
-      .post(`${PREAPPROVED_URL}/${id}`, body)
-      .then(() => {
-        isLoading.value = false
-        addNewTravelDialog.value = false
-        emit("updateTable")
-      })
-      .catch((e) => {
-        isLoading.value = false
-        console.log(e)
-      })
-  }
-}
-
-const store = useStore()
-
-function initForm() {
-  const userDept = store.state.auth.department
-  lockDepartment.value = !isAdmin.value || props.type != "Add New"
-
-  initStates()
-  initDepartments()
-
-  profiles.value = props.type == "Add New" ? [] : props.travelRequest.profiles
-  unknownDate.value = props.type == "Add New" ? false : props.travelRequest.isOpenForAnyDate
-  cost.value = props.type == "Add New" ? "" : props.travelRequest.estimatedCost
-  reason.value = props.type == "Add New" ? "" : props.travelRequest.reason
-  startDate.value = props.type == "Add New" ? "" : props.travelRequest.startDate
-  endDate.value = props.type == "Add New" ? "" : props.travelRequest.endDate
-  department.value = props.type == "Add New" ? userDept : props.travelRequest.department
-  branch.value = props.type == "Add New" ? "" : props.travelRequest.branch
-  undefinedTraveller.value =
-    props.type == "Add New" ? false : props.travelRequest.isOpenForAnyTraveler
-  undefinedTravellerHint.value = ""
-  profilesNum.value = props.type == "Add New" ? null : props.travelRequest.numberTravelers
-  anticipatedMonth.value = props.type == "Add New" ? "" : props.travelRequest.month
-  travellerNotes.value = props.type == "Add New" ? "" : props.travelRequest.travelerNotes
-  travellerDialog.value = false
-  adName.value = ""
-  deleteDialog.value = false
-
-  readonly.value = props.type != "Add New" && props.type != "Edit"
-
-  if (props.type != "Add New") {
-    departmentChanged(branch.value)
-  } else {
-    departmentChanged()
-  }
-
-  loadingData.value = false
-  showApproval.value = false
-  approved.value =
-    props.travelRequest?.status === TRAVEL_AUTHORIZATION_PRE_APPROVAL_STATUSES.APPROVED
-  approvedBy.value = ""
-  approvalDate.value = ""
-
-  if (
-    props.travelRequest?.submissionId &&
-    (props.travelRequest.status === TRAVEL_AUTHORIZATION_PRE_APPROVAL_STATUSES.APPROVED ||
-      props.travelRequest.status === TRAVEL_AUTHORIZATION_PRE_APPROVAL_STATUSES.DECLINED)
-  ) {
-    initSubmission(props.travelRequest.submissionId)
-  }
-}
-
-function initStates() {
-  adNameErr.value = false
-  undefinedTravellerHint.value = ""
-  for (const key of Object.keys(state.value)) {
-    state.value[key] = false
-  }
-}
-
-function initDepartments() {
-  departmentList.value = []
-  const depts = store.state.preapproved.departmentBranch
-  for (const key of Object.keys(depts)) {
-    departmentList.value.push({
-      name: key,
-    })
-  }
-}
-
-async function initSubmission(id) {
-  return http
-    .get(`${PREAPPROVED_URL}/submissions/${id}`)
-    .then((res) => {
-      showApproval.value =
-        res.data.status === TRAVEL_AUTHORIZATION_PRE_APPROVAL_SUBMISSION_STATUSES.FINISHED
-      approvedBy.value = res.data.approvedBy
-      approvalDate.value = res.data.approvalDate
-    })
-    .catch((e) => {
-      console.log(e)
-    })
-}
-
-function departmentChanged(branch) {
-  state.value.departmentErr = false
-  branch.value = branch ? branch : ""
-  const depts = store.state.preapproved.departmentBranch
-  if (department.value) {
-    branchList.value = depts[department.value]?.branches || []
-  } else {
-    branchList.value = []
-  }
-}
-
-function removeTraveller(item) {
-  profiles.value = profiles.value.filter(
-    (profile) => !(profile.profileName == item.profileName && profile.department == item.department)
-  )
-  if (profiles.value.length == 0 && undefinedTraveller.value) {
-    profilesNum.value = null
-  }
-}
+const isLoading = ref(false)
 
 useBreadcrumbs([
   {
