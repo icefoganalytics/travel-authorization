bfe74ac08afa3e19856c1c59053b77e6ed434eb7
Marlen Brunner <klondikemarlen@gmail.com>
2025-03-27 14:41:21 -0700

    :ok_hand: Drop ability to add requests during pre-approval submission.
    Why? It can be understood that a user will select the requests they want before submitting.
    If a user wants to add something after submission, it more sense to do this from a UI for editing the pre-approval submission,
    rather that from the submission dialog.

--- web/src/components/travel-authorization-pre-approvals/TravelAuthorizationPreApprovalSubmissionDialog.vue
+++ web/src/components/travel-authorization-pre-approvals/TravelAuthorizationPreApprovalSubmissionDialog.vue
@@ -12,114 +12,6 @@
       @submit.prevent="submitTravelRequest(TRAVEL_AUTHORIZATION_PRE_APPROVAL_STATUSES.SUBMITTED)"
     >
       <v-row>
-        <v-col class="d-flex justify-end">
-          <v-btn
-            class="my-0"
-            color="primary"
-            @click="openAddTravel"
-          >
-            Add Request
-          </v-btn>
-
-          <!-- TODO: replace with search? -->
-          <v-dialog
-            v-model="addTravelDialog"
-            persistent
-            max-width="900px"
-          >
-            <v-card>
-              <v-card-title
-                class="primary"
-                style="border-bottom: 1px solid black"
-              >
-                <div class="text-h5">Requests</div>
-              </v-card-title>
-
-              <v-card-text>
-                <v-data-table
-                  v-model="newSelectedRequests"
-                  :headers="addTravelHeaders"
-                  :items="remainingTravelRequests"
-                  :items-per-page="5"
-                  class="elevation-1 mt-5"
-                  show-select
-                  single-select
-                >
-                  <template #item.name="{ item }">
-                    <template v-if="item.profiles.length === 0"> Unspecified </template>
-                    <template v-else-if="item.profiles.length === 1">
-                      {{ item.profiles[0].profileName.replace(".", " ") }}
-                    </template>
-                    <v-tooltip
-                      v-else
-                      top
-                      color="primary"
-                    >
-                      <template #activator="{ on }">
-                        <div v-on="on">
-                          <span>
-                            {{ item.profiles[0].profileName.replace(".", " ") }}
-                          </span>
-                          <span>, ... </span>
-                        </div>
-                      </template>
-                      <span
-                        ><div
-                          v-for="(profile, index) in item.profiles"
-                          :key="index"
-                        >
-                          {{ profile.profileName.replace(".", " ") }}
-                        </div></span
-                      >
-                    </v-tooltip>
-                  </template>
-
-                  <template #item.travelDate="{ item }">
-                    <div v-if="item.isOpenForAnyDate">
-                      {{ item.month }}
-                    </div>
-                    <div v-else>
-                      <div>
-                        <!-- eslint-disable-next-line vue/no-parsing-error -->
-                        {{ item.startDate | beautifyDate }}
-                        to
-                      </div>
-                      <div>
-                        <!-- eslint-disable-next-line vue/no-parsing-error -->
-                        {{ item.endDate | beautifyDate }}
-                      </div>
-                    </div>
-                  </template>
-
-                  <template #item.status="{ item }">
-                    <div v-if="item.submissionId != submissionId">
-                      {{ item.status }}
-                    </div>
-                  </template>
-                </v-data-table>
-              </v-card-text>
-
-              <v-card-actions>
-                <v-btn
-                  color="grey darken-5"
-                  @click="addTravelDialog = false"
-                >
-                  Cancel
-                </v-btn>
-                <v-btn
-                  class="ml-auto"
-                  color="green darken-1"
-                  @click="addTravel"
-                >
-                  Add
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-col>
-      </v-row>
-
-      <v-row>
         <v-col>
           <!-- TODO: move to component? -->
           <v-data-table
@@ -240,57 +132,6 @@ const { travelAuthorizationPreApprovals } = useTravelAuthorizationPreApprovals(
 
 const canDelete = computed(() => travelAuthorizationPreApprovals.value.length > 1)
 
-const addTravelHeaders = ref([
-  {
-    text: "Name",
-    value: "name",
-    class: "blue-grey lighten-4",
-  },
-  {
-    text: "Department",
-    value: "department",
-    class: "blue-grey lighten-4",
-  },
-  {
-    text: "Branch",
-    value: "branch",
-    class: "blue-grey lighten-4",
-  },
-
-  {
-    text: "Location",
-    value: "location",
-    class: "blue-grey lighten-4",
-  },
-  {
-    text: "Purpose Type",
-    value: "purpose",
-    class: "blue-grey lighten-4",
-  },
-  // { text: 'Reason',       value: 'reason',      class: 'blue-grey lighten-4' },
-  {
-    text: "Status",
-    value: "status",
-    class: "blue-grey lighten-4",
-    cellClass: "text-h6 red--text",
-  },
-])
-
-const newSelectedRequests = ref([])
-const addTravelDialog = ref(false)
-const isSubmitting = ref(false)
-
-const remainingTravelRequests = computed(() => {
-  const currentIDs = travelAuthorizationPreApprovals.value.map((req) => req.id)
-  const currentDept = travelAuthorizationPreApprovals.value[0]?.department
-  return travelAuthorizationPreApprovals.value.filter(
-    (req) =>
-      !currentIDs.includes(req.id) &&
-      (req.status == null || req.status === TRAVEL_AUTHORIZATION_PRE_APPROVAL_STATUSES.DRAFT) &&
-      (req.department == currentDept || currentIDs.length == 0)
-  )
-})
-
 function removePreApprovalRequest(travelAuthorizationPreApprovalId) {
   const newTravelAuthorizationPreApprovalIds = travelAuthorizationPreApprovalIds.value.filter(
     (id) => id !== travelAuthorizationPreApprovalId
@@ -298,18 +139,7 @@ function removePreApprovalRequest(travelAuthorizationPreApprovalId) {
   travelAuthorizationPreApprovalIds.value = newTravelAuthorizationPreApprovalIds
 }
 
-function openAddTravel() {
-  newSelectedRequests.value = []
-  addTravelDialog.value = true
-}
-
-function addTravel() {
-  travelAuthorizationPreApprovals.value = [
-    ...travelAuthorizationPreApprovals.value,
-    ...newSelectedRequests.value,
-  ]
-  addTravelDialog.value = false
-}
+const isSubmitting = ref(false)
 
 async function submitTravelRequest(status) {
   const currentIDs = travelAuthorizationPreApprovals.value.map((req) => req.id)
