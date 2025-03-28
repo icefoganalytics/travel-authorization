13dbc92366b4b23eeeeebd09eb324b0115eaa49f
Marlen Brunner <klondikemarlen@gmail.com>
2025-03-28 10:10:20 -0700

    :recycle: Rework travel pre-approval submissions page using dedicated table component.
    Why? Easier re-use and readability.

--- web/src/pages/travel-pre-approvals/TravelPreApprovalSubmissionsPage.vue
+++ web/src/pages/travel-pre-approvals/TravelPreApprovalSubmissionsPage.vue
@@ -1,107 +1,55 @@
 <template>
-  <div class="mt-15 mx-10 mb-5">
-    <v-data-table
-      :headers="headers"
-      :items="travelAuthorizationPreApprovalSubmissions"
-      :items-per-page="5"
-      class="elevation-1"
-    >
-      <!-- eslint-disable-next-line vue/no-unused-vars -->
-      <template #item.submissionDate="{ item }">
-        <!-- eslint-disable-next-line vue/no-parsing-error -->
-        {{ item.submissionDate | beautifyDate }}
-      </template>
-      <template #item.location="{ item }">
-        {{ item.preApprovals.map((p) => p.location).join(" - ") }}
-      </template>
-      <template #item.edit="{ item }">
-        <v-row>
-          <div style="width: 4.5rem">
-            <SubmitTravel
-              v-if="item.status === TRAVEL_AUTHORIZATION_PRE_APPROVAL_SUBMISSION_STATUSES.DRAFT"
-              :submission-id="item.preTSubID"
-              :edit-button="true"
-              button-name="Edit"
-              :travel-requests="travelAuthorizationPreApprovals"
-              :selected-requests="item.preApprovals"
-              @updateTable="refresh"
-            />
-          </div>
-          <div style="width: 6.75rem">
-            <ApproveTravel
-              v-if="item.status === TRAVEL_AUTHORIZATION_PRE_APPROVAL_SUBMISSION_STATUSES.SUBMITTED"
-              :travel-requests="item.preApprovals"
-              :submission-id="item.preTSubID"
-              @updateTable="refresh"
-            />
-          </div>
-          <div style="width: 5.75rem">
-            <PrintReport
-              :id="item.preTSubID"
-              :travel-requests="item.preApprovals"
-              :button-inside-table="true"
-              button-name="Print"
-            />
-          </div>
-        </v-row>
-      </template>
-    </v-data-table>
-  </div>
+  <TravelAuthorizationPreApprovalSubmissionsDataTable
+    ref="travelAuthorizationPreApprovalSubmissionsDataTable"
+  >
+    <template #item.actions="{ item }">
+      <div class="d-flex justify-end gap-4">
+        <SubmitTravel
+          v-if="item.status === TRAVEL_AUTHORIZATION_PRE_APPROVAL_SUBMISSION_STATUSES.DRAFT"
+          :submission-id="item.preTSubID"
+          :edit-button="true"
+          button-name="Edit"
+          :travel-requests="travelAuthorizationPreApprovals"
+          :selected-requests="item.preApprovals"
+          @updateTable="refresh"
+        />
+        <ApproveTravel
+          v-if="item.status === TRAVEL_AUTHORIZATION_PRE_APPROVAL_SUBMISSION_STATUSES.SUBMITTED"
+          :travel-requests="item.preApprovals"
+          :submission-id="item.preTSubID"
+          @updateTable="refresh"
+        />
+        <PrintReport
+          :id="item.preTSubID"
+          :travel-requests="item.preApprovals"
+          :button-inside-table="true"
+          button-name="Print"
+        />
+      </div>
+    </template>
+  </TravelAuthorizationPreApprovalSubmissionsDataTable>
 </template>
 
 <script setup>
+import { ref } from "vue"
+
 import { TRAVEL_AUTHORIZATION_PRE_APPROVAL_SUBMISSION_STATUSES } from "@/api/travel-authorization-pre-approval-submissions-api"
 import useBreadcrumbs from "@/use/use-breadcrumbs"
 import useTravelAuthorizationPreApprovals from "@/use/use-travel-authorization-pre-approvals"
-import useTravelAuthorizationPreApprovalSubmissions from "@/use/use-travel-authorization-pre-approval-submissions"
 
 import PrintReport from "@/modules/preapproved/views/Common/PrintReport.vue"
 import SubmitTravel from "@/modules/preapproved/views/Common/SubmitTravel.vue"
 import ApproveTravel from "@/modules/preapproved/views/Submissions/ApproveTravel.vue"
-
-const headers = [
-  {
-    text: "Submission Date",
-    value: "submissionDate",
-    class: "blue-grey lighten-4",
-  },
-  {
-    text: "Department",
-    value: "department",
-    class: "blue-grey lighten-4",
-  },
-  {
-    text: "Location",
-    value: "location",
-    class: "blue-grey lighten-4",
-  },
-  {
-    text: "Submitter",
-    value: "submitter",
-    class: "blue-grey lighten-4",
-  },
-  {
-    text: "Status",
-    value: "status",
-    class: "blue-grey lighten-4",
-  },
-  {
-    text: "",
-    sortable: false,
-    value: "edit",
-    class: "blue-grey lighten-4",
-    width: "18rem",
-  },
-]
-
-const { travelAuthorizationPreApprovalSubmissions, refresh: refreshPreApprovalSubmissions } =
-  useTravelAuthorizationPreApprovalSubmissions()
+import TravelAuthorizationPreApprovalSubmissionsDataTable from "@/components/travel-authorization-pre-approval-submissions/TravelAuthorizationPreApprovalSubmissionsDataTable.vue"
 
 const { travelAuthorizationPreApprovals, refresh: refreshPreApprovals } =
   useTravelAuthorizationPreApprovals()
 
+/** @type {import("vue").Ref<InstanceType<typeof TravelAuthorizationPreApprovalSubmissionsDataTable> | null>} */
+const travelAuthorizationPreApprovalSubmissionsDataTable = ref(null)
+
 async function refresh() {
-  await refreshPreApprovalSubmissions()
+  await travelAuthorizationPreApprovalSubmissionsDataTable.value?.refresh()
   await refreshPreApprovals()
 }
 
@@ -122,7 +70,7 @@ useBreadcrumbs([
 </script>
 
 <style scoped>
-::v-deep(tbody tr:nth-of-type(even)) {
-  background-color: rgba(0, 0, 0, 0.05);
+.gap-4 {
+  gap: 1rem; /* 16px */
 }
 </style>
