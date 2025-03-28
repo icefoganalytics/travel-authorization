4da55db6eac5e5677b1a7138829372b3dc2b83b3
Marlen Brunner <klondikemarlen@gmail.com>
2025-03-28 11:35:05 -0700

    :art: Make SummaryHeaderPanel reflow better.

--- web/src/components/travel-authorizations/SummaryHeaderPanel.vue
+++ web/src/components/travel-authorizations/SummaryHeaderPanel.vue
@@ -3,46 +3,33 @@
     <v-card-text>
       <v-row dense>
         <v-col
-          cols="12"
-          md="2"
-          class="d-flex align-center"
+          class="d-flex align-center justify-center justify-md-start"
+          :cols="mdAndUp ? undefined : 12"
         >
           <h2 class="mb-0">Travel</h2>
         </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
+        <v-col :cols="mdAndUp ? undefined : 12">
           <DescriptionElement
             label="Purpose"
             :value="purposeText"
             :vertical="mdAndUp"
           />
         </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
+        <v-col :cols="mdAndUp ? undefined : 12">
           <LocationDescriptionElement
             label="Final Destination"
             :location-id="finalDestination.locationId"
             :vertical="mdAndUp"
           />
         </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
+        <v-col :cols="mdAndUp ? undefined : 12">
           <DescriptionElement
             label="Depart"
             :value="initialDestination.departureDate"
             :vertical="mdAndUp"
           />
         </v-col>
-        <v-col
-          cols="12"
-          md="2"
-        >
+        <v-col :cols="mdAndUp ? undefined : 12">
           <DescriptionElement
             label="Return"
             :value="finalDestinationDepartureDate"
@@ -50,9 +37,8 @@
           />
         </v-col>
         <v-col
-          cols="12"
-          md="2"
-          class="d-flex align-center"
+          class="d-flex align-center justify-center justify-md-start"
+          :cols="mdAndUp ? undefined : 12"
         >
           <UserChip :user-id="currentUser.id" />
         </v-col>
