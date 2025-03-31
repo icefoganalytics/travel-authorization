88b29d51b7a52bfa1002f264af2bf332dce274b9
Marlen Brunner <klondikemarlen@gmail.com>
2025-03-31 14:52:57 -0700

    :sparkles: Add pre-approval document download button UI.

---
+++ web/src/components/travel-authorization-pre-approval-documents/DownloadApprovalDocumentFormBtn.vue
@@ -0,0 +1,90 @@
+<template>
+  <form
+    ref="form"
+    :action="downloadUrl"
+    method="post"
+    target="_blank"
+    @submit.prevent="getAccessTokenAndSubmit"
+  >
+    <input
+      type="hidden"
+      name="FORM_API_BEARER_TOKEN"
+      :value="accessToken"
+    />
+    <v-btn
+      :loading="isDownloading"
+      v-bind="buttonPropsWithDefaults"
+      type="submit"
+    >
+      <slot>{{ buttonText }}</slot>
+      <v-icon right>mdi-download</v-icon>
+    </v-btn>
+  </form>
+</template>
+
+<script setup>
+import { computed, ref, nextTick } from "vue"
+import { isNil, merge } from "lodash"
+
+import { useAuth0 } from "@/plugins/auth0-plugin"
+
+import { API_BASE_URL } from "@/config"
+
+const props = defineProps({
+  travelAuthorizationPreApprovalDocumentId: {
+    type: Number,
+    required: true,
+  },
+  buttonText: {
+    type: String,
+    default: "Download",
+  },
+  buttonProps: {
+    type: Object,
+    default: () => ({}),
+  },
+})
+
+const buttonPropsWithDefaults = computed(() =>
+  merge(
+    {},
+    {
+      outlined: true,
+      xLarge: true,
+      color: "info",
+    },
+    props.buttonProps
+  )
+)
+
+const { getAccessTokenSilently } = useAuth0()
+
+/** @type {import('vue').Ref<HTMLFormElement | null>} */
+const form = ref(null)
+const accessToken = ref(null)
+
+const downloadUrl = computed(() => {
+  return `${API_BASE_URL}/download/travel-authorization-pre-approval-documents/${props.travelAuthorizationPreApprovalDocumentId}`
+})
+
+const isDownloading = ref(false)
+
+async function getAccessTokenAndSubmit() {
+  if (isNil(form.value)) {
+    throw new Error("Form element is not available")
+  }
+
+  isDownloading.value = true
+  try {
+    accessToken.value = await getAccessTokenSilently()
+    await nextTick() // Wait for accessToken to be updated in the DOM
+
+    form.value.submit()
+  } catch (error) {
+    console.error("Error fetching new access token:", error)
+    throw error
+  } finally {
+    isDownloading.value = false
+  }
+}
+</script>
--- web/src/components/travel-authorization-pre-approval-submissions/TravelAuthorizationPreApprovalSubmissionCard.vue
+++ web/src/components/travel-authorization-pre-approval-submissions/TravelAuthorizationPreApprovalSubmissionCard.vue
@@ -13,15 +13,17 @@
       :key="travelAuthorizationPreApprovalDocument.id"
     >
       <v-col cols="12">
-        <v-btn
-          :loading="isDownloading"
-          color="transparent"
-          @click="downloadPdf(travelAuthorizationPreApprovalDocument.id)"
+        <DescriptionElement
+          label="Approval Document"
+          vertical
         >
-          <span class="text-h6 primary--text text-decoration-underline">
-            <b>{{ travelAuthorizationPreApprovalDocument.name }}</b>
-          </span>
-        </v-btn>
+          <DownloadApprovalDocumentFormBtn
+            :travel-authorization-pre-approval-document-id="
+              travelAuthorizationPreApprovalDocument.id
+            "
+            :button-text="travelAuthorizationPreApprovalDocument.name"
+          />
+        </DescriptionElement>
       </v-col>
       <v-col
         cols="12"
@@ -72,15 +74,17 @@
       :key="travelAuthorizationPreApprovalDocument.id"
     >
       <v-col cols="12">
-        <v-btn
-          :loading="isDownloading"
-          color="transparent"
-          @click="downloadPdf(travelAuthorizationPreApprovalDocument.id)"
+        <DescriptionElement
+          label="Signature Document"
+          vertical
         >
-          <span class="text-h6 primary--text text-decoration-underline">
-            <b>{{ travelAuthorizationPreApprovalDocument.name }}</b>
-          </span>
-        </v-btn>
+          <DownloadApprovalDocumentFormBtn
+            :travel-authorization-pre-approval-document-id="
+              travelAuthorizationPreApprovalDocument.id
+            "
+            :button-text="travelAuthorizationPreApprovalDocument.name"
+          />
+        </DescriptionElement>
       </v-col>
       <v-col
         cols="12"
@@ -134,11 +138,8 @@
 </template>
 
 <script setup>
-import { computed, ref, toRefs } from "vue"
+import { computed, toRefs } from "vue"
 import { isNil } from "lodash"
-
-import http from "@/api/http-client"
-import { PREAPPROVED_URL } from "@/urls"
 
 import { formatDate } from "@/utils/formatters"
 
@@ -146,9 +147,10 @@ import useTravelAuthorizationPreApprovalSubmission, {
   TRAVEL_AUTHORIZATION_PRE_APPROVAL_SUBMISSION_STATUSES,
 } from "@/use/use-travel-authorization-pre-approval-submission"
 
-import UserChip from "@/components/users/UserChip.vue"
 import DescriptionElement from "@/components/common/DescriptionElement.vue"
 import HeaderActionsFormCard from "@/components/common/HeaderActionsFormCard.vue"
+import DownloadApprovalDocumentFormBtn from "@/components/travel-authorization-pre-approval-documents/DownloadApprovalDocumentFormBtn.vue"
+import UserChip from "@/components/users/UserChip.vue"
 
 const props = defineProps({
   travelAuthorizationPreApprovalSubmissionId: {
@@ -177,37 +179,4 @@ const isDeclined = computed(
 const travelAuthorizationPreApprovalDocuments = computed(
   () => travelAuthorizationPreApprovalSubmission.value?.documents
 )
-
-const isDownloading = ref(false)
-
-// TODO: update this to use new endpoint, preferably via form post redirect to get best user experience with the least code
-// See https://github.com/icefoganalytics/internal-data-portal/blob/0eb01fff60c6b5d72b060f89e92cf15336225531/web/src/components/dataset-entries/DownloadAsCsvButton.vue#L18
-// Or use an http only cookie for auth, but that will take a lot more setup.
-async function downloadPdf() {
-  isDownloading.value = true
-  try {
-    const header = {
-      responseType: "application/pdf",
-      headers: {
-        "Content-Type": "application/text",
-      },
-    }
-
-    const { data } = await http.get(
-      `${PREAPPROVED_URL}/document/${travelAuthorizationPreApprovalSubmissionId.value}`,
-      header
-    )
-    isDownloading.value = false
-    const link = document.createElement("a")
-    link.href = data
-    document.body.appendChild(link)
-    link.download = "approval_doc.pdf"
-    link.click()
-    setTimeout(() => URL.revokeObjectURL(link.href), 1000)
-  } catch (error) {
-    console.error(`Failed to download PDF: ${error}`)
-  } finally {
-    isDownloading.value = false
-  }
-}
 </script>
