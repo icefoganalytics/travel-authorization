4cd4f38f1566643974fb61da793066fe147f4f2a
Marlen Brunner <klondikemarlen@gmail.com>
2025-04-02 10:29:41 -0700

    :art: Refactor Travel Pre-Approvals page to use standardized card component.

--- web/src/pages/TravelPreApprovalsPage.vue
+++ web/src/pages/TravelPreApprovalsPage.vue
@@ -1,33 +1,34 @@
 <template>
-  <v-card>
-    <v-card-title>
-      <h2>Travel Pre-Approvals</h2>
-    </v-card-title>
-    <v-card-text>
-      <v-tabs active-class="primary--text teal lighten-5">
-        <v-tab
-          :to="{
-            name: 'travel-pre-approvals/TravelPreApprovalRequestsPage',
-          }"
-        >
-          Requests
-        </v-tab>
-        <v-tab
-          :to="{
-            name: 'travel-pre-approvals/TravelPreApprovalSubmissionsPage',
-          }"
-        >
-          Submissions
-        </v-tab>
-      </v-tabs>
+  <HeaderActionsCard
+    title="Travel Pre-Approvals"
+    header-tag="h2"
+    header-class="text-h4 mb-0"
+  >
+    <v-tabs active-class="primary--text teal lighten-5">
+      <v-tab
+        :to="{
+          name: 'travel-pre-approvals/TravelPreApprovalRequestsPage',
+        }"
+      >
+        Requests
+      </v-tab>
+      <v-tab
+        :to="{
+          name: 'travel-pre-approvals/TravelPreApprovalSubmissionsPage',
+        }"
+      >
+        Submissions
+      </v-tab>
+    </v-tabs>
 
-      <router-view />
-    </v-card-text>
-  </v-card>
+    <router-view />
+  </HeaderActionsCard>
 </template>
 
 <script setup>
 import useBreadcrumbs from "@/use/use-breadcrumbs"
+
+import HeaderActionsCard from "@/components/common/HeaderActionsCard.vue"
 
 useBreadcrumbs([
   {
