ac80168fb046ff2c4b107c72dd58456ebeaf3a23
Marlen Brunner <klondikemarlen@gmail.com>
2025-04-09 15:29:54 -0700

    :white_check_mark: Add more exhaustive tests for YG employee group SyncService.

--- api/tests/services/yg-employee-groups/sync-service.test.ts
+++ api/tests/services/yg-employee-groups/sync-service.test.ts
@@ -5,7 +5,7 @@ import yukonGovernmentIntegration, {
 import { YgEmployeeGroup } from "@/models"
 import { SyncService } from "@/services/yg-employee-groups"
 
-import { loadTestData } from "@/support"
+import { loadTestData, testWithCustomLogLevel } from "@/support"
 
 vi.mock("@/integrations/yukon-government-integration", () => {
   const yukonGovernmentIntegrationMock = {
@@ -40,6 +40,96 @@ describe("api/src/services/yg-employee-groups/sync-service.ts", () => {
         const ygEmployeeGroupCount = await YgEmployeeGroup.count()
         expect(ygEmployeeGroupCount).toBe(709)
       })
+
+      test("when yukon government integration returns employee groups, it creates the expected number of departments", async () => {
+        // Arrange
+        const apiResponse = loadTestData(
+          "yukon-government-api-directory-divisions-response-2025-04-09.json"
+        ) as {
+          divisions: YukonGovernmentDivision[]
+          count: number
+        }
+        yukonGovernmentIntegrationMock.fetchDivisions.mockResolvedValue(apiResponse)
+
+        // Act
+        await SyncService.perform()
+
+        // Assert
+        const departmentCount = await YgEmployeeGroup.scope("isDepartment").count()
+        expect(departmentCount).toBe(26)
+      })
+
+      test("when yukon government integration returns employee groups, it creates the expected number of divisions", async () => {
+        // Arrange
+        const apiResponse = loadTestData(
+          "yukon-government-api-directory-divisions-response-2025-04-09.json"
+        ) as {
+          divisions: YukonGovernmentDivision[]
+          count: number
+        }
+        yukonGovernmentIntegrationMock.fetchDivisions.mockResolvedValue(apiResponse)
+
+        // Act
+        await SyncService.perform()
+
+        // Assert
+        const divisionCount = await YgEmployeeGroup.scope("isDivision").count()
+        expect(divisionCount).toBe(102)
+      })
+
+      test("when yukon government integration returns employee groups, it creates the expected number of branches", async () => {
+        // Arrange
+        const apiResponse = loadTestData(
+          "yukon-government-api-directory-divisions-response-2025-04-09.json"
+        ) as {
+          divisions: YukonGovernmentDivision[]
+          count: number
+        }
+        yukonGovernmentIntegrationMock.fetchDivisions.mockResolvedValue(apiResponse)
+
+        // Act
+        await SyncService.perform()
+
+        // Assert
+        const branchCount = await YgEmployeeGroup.scope("isBranch").count()
+        expect(branchCount).toBe(264)
+      })
+
+      test("when yukon government integration returns employee groups, it creates the expected number of units", async () => {
+        // Arrange
+        const apiResponse = loadTestData(
+          "yukon-government-api-directory-divisions-response-2025-04-09.json"
+        ) as {
+          divisions: YukonGovernmentDivision[]
+          count: number
+        }
+        yukonGovernmentIntegrationMock.fetchDivisions.mockResolvedValue(apiResponse)
+
+        // Act
+        await SyncService.perform()
+
+        // Assert
+        const unitCount = await YgEmployeeGroup.scope("isUnit").count()
+        expect(unitCount).toBe(317)
+      })
+
+      testWithCustomLogLevel(
+        "when yukon government integration errors, catches the error",
+        async ({ setLogLevel }) => {
+          // Arrange
+          setLogLevel("silent")
+
+          yukonGovernmentIntegrationMock.fetchDivisions.mockRejectedValue(
+            new Error("Yukon government integration error")
+          )
+
+          // Assert
+          await expect(
+            // Act
+            SyncService.perform()
+          ).resolves.toBeUndefined()
+        }
+      )
     })
   })
 })
