ee06822194b9ebe0f8c6f947edf3a8c1fb8bda8e
Marlen Brunner <klondikemarlen@gmail.com>
2025-04-10 15:27:51 -0700

    :cherry_blossom: Make trip purpose details step display look better at various screen sizes.
    Its a bit big now, but at least it doesn't compress the "Objectives" text to the point that it's unreadable.

--- web/src/components/travel-authorizations/PurposeCard.vue
+++ web/src/components/travel-authorizations/PurposeCard.vue
@@ -1,100 +1,85 @@
 <template>
-  <v-card>
-    <v-card-title> Purpose </v-card-title>
-    <v-card-text>
-      <v-row>
-        <v-col
-          cols="12"
-          md="6"
-        >
-          <v-row dense>
-            <v-col
-              cols="12"
-              md="6"
-            >
-              <v-text-field
-                :value="purposeText"
-                :loading="isLoadingTravelPurposes"
-                label="Purpose"
-                dense
-                outlined
-                readonly
-                append-icon="mdi-lock"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field
-                :value="travelAuthorization.eventName"
-                label="Name of meeting/conference, mission, trade fair or course"
-                dense
-                outlined
-                readonly
-                append-icon="mdi-lock"
-              ></v-text-field>
-            </v-col>
-            <v-col
-              cols="12"
-              md="3"
-            >
-              <!-- Depending on in territory flag we will load a different list of destinations -->
-              <v-checkbox
-                :value="travelAuthorization.allTravelWithinTerritory"
-                label="In Territory?"
-                dense
-                readonly
-                append-icon="mdi-lock"
-              >
-              </v-checkbox>
-            </v-col>
-            <v-col
-              cols="12"
-              md="9"
-            >
-              <LocationReadonlyTextField
-                :location-id="finalDestination.locationId"
-                label="Final Destination"
-                dense
-                outlined
-              />
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-col
-          cols="12"
-          md="6"
-        >
-          <v-row>
-            <v-col
-              cols="12"
-              md="3"
-            >
-              <h3>Objectives</h3>
-              <ul>
-                <li>Purpose of attendance</li>
-                <li>Relevance and anticipated benefits to branch and Government of Yukon</li>
-              </ul>
-            </v-col>
-            <v-col
-              cols="12"
-              md="9"
-            >
-              <v-textarea
-                :value="travelAuthorization.benefits"
-                label="Objectives"
-                rows="10"
-                auto-grow
-                dense
-                outlined
-                readonly
-                append-icon="mdi-lock"
-              >
-              </v-textarea>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-card-text>
-  </v-card>
+  <HeaderActionsCard title="Purpose">
+    <v-row>
+      <v-col
+        cols="12"
+        md="6"
+      >
+        <v-text-field
+          :value="purposeText"
+          :loading="isLoadingTravelPurposes"
+          label="Purpose"
+          dense
+          outlined
+          readonly
+          append-icon="mdi-lock"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        xl="6"
+      >
+        <v-text-field
+          :value="travelAuthorization.eventName"
+          label="Name of meeting/conference, mission, trade fair or course"
+          dense
+          outlined
+          readonly
+          append-icon="mdi-lock"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        md="3"
+        xl="2"
+      >
+        <!-- Depending on in territory flag we will load a different list of destinations -->
+        <v-checkbox
+          :value="travelAuthorization.allTravelWithinTerritory"
+          label="In Territory?"
+          dense
+          readonly
+          append-icon="mdi-lock"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        md="9"
+        lg="6"
+        xl="4"
+      >
+        <LocationReadonlyTextField
+          :location-id="finalDestination.locationId"
+          label="Final Destination"
+          dense
+          outlined
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h3>Objectives</h3>
+        <ul>
+          <li>Purpose of attendance</li>
+          <li>Relevance and anticipated benefits to branch and Government of Yukon</li>
+        </ul>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-textarea
+          :value="travelAuthorization.benefits"
+          label="Objectives"
+          rows="10"
+          auto-grow
+          dense
+          outlined
+          readonly
+          append-icon="mdi-lock"
+        />
+      </v-col>
+    </v-row>
+  </HeaderActionsCard>
 </template>
 
 <script setup>
@@ -105,6 +90,7 @@ import { MAX_PER_PAGE } from "@/api/base-api"
 import useTravelAuthorization from "@/use/use-travel-authorization"
 import useTravelPurposes from "@/use/use-travel-purposes"
 
+import HeaderActionsCard from "@/components/common/HeaderActionsCard.vue"
 import LocationReadonlyTextField from "@/components/locations/LocationReadonlyTextField.vue"
 
 const props = defineProps({
