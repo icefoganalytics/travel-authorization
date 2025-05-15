4c969555655bb47e8ff301f0127597fd0cd545fc
Marlen Brunner <klondikemarlen@gmail.com>
2025-05-15 09:16:13 -0700

    :construction: Standardize travel authorization layout to match other travel authorization layouts.

--- web/src/layouts/travel-authorizations/TravelAuthorizationLayout.vue
+++ web/src/layouts/travel-authorizations/TravelAuthorizationLayout.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <Breadcrumbs />
-
-    <SummaryHeaderPanel :travel-authorization-id="travelAuthorizationIdAsNumber" />
+    <SummaryHeaderPanel
+      ref="summaryHeaderPanel"
+      :travel-authorization-id="travelAuthorizationIdAsNumber"
+    />
 
     <v-tabs>
       <v-tab
@@ -35,15 +36,25 @@
       <!-- TODO: add in any tabs that you can normally see in manage mode -->
     </v-tabs>
 
-    <router-view></router-view>
+    <router-view @updated="refresh"></router-view>
+
+    <v-row class="mt-md-10 mt-5">
+      <v-col>
+        <TravelAuthorizationActionLogsTable
+          :travel-authorization-id="travelAuthorizationIdAsNumber"
+        />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue"
+import { computed, ref } from "vue"
 
-import Breadcrumbs from "@/components/Breadcrumbs.vue"
+import useBreadcrumbs from "@/use/use-breadcrumbs"
+
 import SummaryHeaderPanel from "@/components/travel-authorizations/SummaryHeaderPanel.vue"
+import TravelAuthorizationActionLogsTable from "@/components/travel-authorization-action-logs/TravelAuthorizationActionLogsTable.vue"
 
 const props = defineProps({
   travelAuthorizationId: {
@@ -53,4 +64,20 @@ const props = defineProps({
 })
 
 const travelAuthorizationIdAsNumber = computed(() => parseInt(props.travelAuthorizationId))
+
+/** @type {import('vue').Ref<InstanceType<typeof SummaryHeaderPanel> | null>} */
+const summaryHeaderPanel = ref(null)
+
+async function refresh() {
+  await summaryHeaderPanel.value?.refresh()
+}
+
+useBreadcrumbs([
+  {
+    text: "Travel Requests",
+    to: {
+      name: "TravelRequests",
+    },
+  },
+])
 </script>
