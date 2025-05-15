f8b4772726fc319e35afc17520909dd93802184f
Marlen Brunner <klondikemarlen@gmail.com>
2025-05-15 09:04:01 -0700

    :recycle: Flattent travel authorization layout structure.
    Move simple tabs inline.

--- web/src/layouts/travel-authorizations/TravelAuthorizationLayout.vue
+++ web/src/layouts/travel-authorizations/TravelAuthorizationLayout.vue
@@ -5,9 +5,33 @@
     <SummaryHeaderPanel :travel-authorization-id="travelAuthorizationIdAsNumber" />
 
     <v-tabs>
-      <DetailsTab :travel-authorization-id="travelAuthorizationIdAsNumber" />
-      <EstimateTab :travel-authorization-id="travelAuthorizationIdAsNumber" />
-      <ExpenseTab :travel-authorization-id="travelAuthorizationIdAsNumber" />
+      <v-tab
+        :to="{
+          name: 'EditTravelAuthorizationDetailsPage',
+          params: {
+            travelAuthorizationId,
+          },
+        }"
+      >
+        Details
+      </v-tab>
+      <v-tab
+        :to="{
+          name: 'EditTravelAuthorizationEstimatePage',
+          params: {
+            travelAuthorizationId,
+          },
+        }"
+        >Estimate</v-tab
+      >
+      <v-tab
+        :to="{
+          name: 'EditTravelAuthorizationExpensePage',
+          params: { travelAuthorizationId },
+        }"
+      >
+        Expense
+      </v-tab>
       <!-- TODO: add in any tabs that you can normally see in manage mode -->
     </v-tabs>
 
@@ -20,10 +44,6 @@ import { computed } from "vue"
 
 import Breadcrumbs from "@/components/Breadcrumbs.vue"
 import SummaryHeaderPanel from "@/components/travel-authorizations/SummaryHeaderPanel.vue"
-
-import DetailsTab from "@/modules/travel-authorizations/components/travel-authorization-layout/DetailsTab.vue"
-import EstimateTab from "@/modules/travel-authorizations/components/travel-authorization-layout/EstimateTab.vue"
-import ExpenseTab from "@/modules/travel-authorizations/components/travel-authorization-layout/ExpenseTab.vue"
 
 const props = defineProps({
   travelAuthorizationId: {
