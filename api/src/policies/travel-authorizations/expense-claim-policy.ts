8736c0b655793fee31e7fcceddebec7f158d139d
Marlen Brunner <klondikemarlen@gmail.com>
2025-05-29 16:16:00 -0700

    :pencil: move comment from policy to relevant UI component.

--- api/src/policies/travel-authorizations/expense-claim-policy.ts
+++ api/src/policies/travel-authorizations/expense-claim-policy.ts
@@ -3,9 +3,6 @@ import BasePolicy from "@/policies/base-policy"
 import { TravelAuthorization } from "@/models"
 
 export class ExpenseClaimPolicy extends BasePolicy<TravelAuthorization> {
-  // I'm not sure if the following logic should be in a policy or just the UI?
-  // Submit becomes enabled with there are more than zero "Coding" rows,
-  // and all expenses have an associated upload/receipt.
   create(): boolean {
     if (this.user.isAdmin) return true
     if (this.record.supervisorEmail === this.user.email) return true
