6690461a7eec3614beff8243a61b9314477155d3
Marlen Brunner <klondikemarlen@gmail.com>
2025-06-06 17:50:19 -0700

    :recycle: Replace all legacy useSnack imports from "plugins" with newer "use" imports.

--- web/src/plugins/snack-plugin.js
+++ web/src/plugins/snack-plugin.js
@@ -1,7 +1,5 @@
 import { useSnack } from "@/use/use-snack"
 
-export { useSnack }
-
 const SnackPlugin = {
   install(VueInstance, defaultOptions = { timeout: 4000 }) {
     VueInstance.prototype.$snack = useSnack(defaultOptions)
