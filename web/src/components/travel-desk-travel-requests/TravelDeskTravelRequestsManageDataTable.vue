1dc858e1fd40eabda41975bb845f3931849649a2
Marlen Brunner <klondikemarlen@gmail.com>
2025-06-20 15:18:53 -0700

    :cherry_blossom: Add travel authorization id number column to travel desk.
    Why? So its actually possible to figure out which travel request I need to review.

--- web/src/components/travel-desk-travel-requests/TravelDeskTravelRequestsManageDataTable.vue
+++ web/src/components/travel-desk-travel-requests/TravelDeskTravelRequestsManageDataTable.vue
@@ -152,6 +152,7 @@ import ExportToCsvButton from "@/components/travel-desk-travel-requests/ExportTo
 import PrintTravelDeskReport from "@/modules/travelDesk/views/Common/PrintTravelDeskReport.vue"
 
 const headers = ref([
+  { text: "TA #", value: "travelAuthorizationId" },
   { text: "Submit Date", value: "createdAt" },
   { text: "Name", value: "userDisplayName", sortable: false },
   { text: "Department", value: "department", sortable: false },
