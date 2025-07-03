175d1771537284e3639cb49fe664446b7ef90627
Marlen Brunner <klondikemarlen@gmail.com>
2025-07-03 11:55:22 -0700

    :construction: Port Expense model over to Sequelize 7 format.

--- api/src/models/expense.ts
+++ api/src/models/expense.ts
@@ -1,20 +1,23 @@
 import {
-  Association,
-  BelongsToCreateAssociationMixin,
-  BelongsToGetAssociationMixin,
-  BelongsToSetAssociationMixin,
-  CreationOptional,
   DataTypes,
-  ForeignKey,
   InferAttributes,
   InferCreationAttributes,
   Model,
-  NonAttribute,
+  type CreationOptional,
+  type NonAttribute,
 } from "@sequelize/core"
+import {
+  Attribute,
+  AutoIncrement,
+  BelongsTo,
+  Default,
+  NotNull,
+  PrimaryKey,
+  Table,
+  ValidateAttribute,
+} from "@sequelize/core/decorators-legacy"
 
-import sequelize from "@/db/db-client"
-
-import TravelAuthorization from "./travel-authorization"
+import TravelAuthorization from "@/models/travel-authorization"
 
 // Keep in sync with web/src/modules/travel-authorizations/components/ExpenseTypeSelect.vue
 export enum ExpenseTypes {
@@ -38,148 +41,120 @@ export enum Types {
   EXPENSE = "Expense",
 }
 
+@Table({
+  tableName: "expenses",
+  paranoid: false,
+  // TODO: consider whether it would be better to use a separate table for uploads
+  // e.g. Rails https://guides.rubyonrails.org/active_storage_overview.html
+  defaultScope: {
+    attributes: { exclude: ["receiptImage"] },
+  },
+})
 export class Expense extends Model<InferAttributes<Expense>, InferCreationAttributes<Expense>> {
   static readonly Types = Types
   static readonly ExpenseTypes = ExpenseTypes
   static readonly CurrencyTypes = ExpenseCurrencyTypes
 
+  @Attribute(DataTypes.INTEGER)
+  @PrimaryKey
+  @AutoIncrement
   declare id: CreationOptional<number>
-  declare travelAuthorizationId: ForeignKey<TravelAuthorization["id"]>
+
+  @Attribute(DataTypes.INTEGER)
+  @NotNull
+  declare travelAuthorizationId: number
+
+  @Attribute(DataTypes.STRING(255))
+  @NotNull
   declare description: string
+
+  @Attribute(DataTypes.DATEONLY)
   declare date: Date | string | null // DATEONLY accepts Date or string, but returns string
+
+  @Attribute(DataTypes.FLOAT)
+  @NotNull
   declare cost: number
+
+  @Attribute(DataTypes.STRING(255))
+  @NotNull
+  @ValidateAttribute({
+    isIn: {
+      args: [Object.values(ExpenseCurrencyTypes)],
+      msg: `Currency must be one of: ${Object.values(ExpenseCurrencyTypes).join(", ")}`,
+    },
+  })
   declare currency: ExpenseCurrencyTypes
+
+  @Attribute(DataTypes.STRING)
+  @NotNull
+  @ValidateAttribute({
+    isIn: {
+      args: [Object.values(Types)],
+      msg: `Type must be one of: ${Object.values(Types).join(", ")}`,
+    },
+  })
   declare type: Types
+
+  @Attribute(DataTypes.BLOB)
+  @ValidateAttribute({
+    // NOTE: docs at https://sequelize.org/docs/v7/models/validations-and-constraints/#attribute-validators
+    // are wrong; this is the correct signature at the moment
+    hasFileSize(value: Buffer | null) {
+      if (value !== null && this.fileSize === null) {
+        throw new Error("fileSize must be set when receiptImage is set")
+      }
+    },
+  })
   declare receiptImage: Buffer | null
+
+  @Attribute(DataTypes.INTEGER)
+  @ValidateAttribute({
+    hasReceiptImage(value: number | null) {
+      if (value !== null && this.receiptImage === null) {
+        throw new Error("receiptImage must be set when fileSize is set")
+      }
+    },
+  })
   declare fileSize: number | null
+
+  @Attribute(DataTypes.STRING(255))
   declare fileName: string | null
+
+  @Attribute(DataTypes.STRING(255))
+  @NotNull
+  @ValidateAttribute({
+    isIn: {
+      args: [Object.values(ExpenseTypes)],
+      msg: `ExpenseType must be one of: ${Object.values(ExpenseTypes).join(", ")}`,
+    },
+  })
   declare expenseType: ExpenseTypes
+
+  @Attribute(DataTypes.DATE)
+  @NotNull
+  @Default(DataTypes.NOW)
   declare createdAt: CreationOptional<Date>
+
+  @Attribute(DataTypes.DATE)
+  @NotNull
+  @Default(DataTypes.NOW)
   declare updatedAt: CreationOptional<Date>
 
-  // https://sequelize.org/docs/v6/other-topics/typescript/#usage
-  // https://sequelize.org/docs/v6/core-concepts/assocs/#special-methodsmixins-added-to-instances
-  // https://sequelize.org/api/v7/types/_sequelize_core.index.belongstocreateassociationmixin
-  declare getTravelAuthorization: BelongsToGetAssociationMixin<TravelAuthorization>
-  declare setTravelAuthorization: BelongsToSetAssociationMixin<
-    TravelAuthorization,
-    TravelAuthorization["id"]
-  >
-  declare createTravelAuthorization: BelongsToCreateAssociationMixin<TravelAuthorization>
-
+  // Associations
+  @BelongsTo(() => TravelAuthorization, {
+    foreignKey: "travelAuthorizationId",
+    inverse: {
+      as: "expenses",
+      type: "hasMany",
+    },
+  })
   declare travelAuthorization?: NonAttribute<TravelAuthorization>
 
-  declare static associations: {
-    travelAuthorization: Association<Expense, TravelAuthorization>
-  }
-
-  static establishAssociations() {
-    this.belongsTo(TravelAuthorization, {
-      as: "travelAuthorization",
-      foreignKey: "travelAuthorizationId",
+  static establishScopes(): void {
+    this.addScope("withReceiptImage", {
+      attributes: { include: ["receiptImage"] },
     })
   }
 }
-
-Expense.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    travelAuthorizationId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "travel_authorizations", // using real table name here
-        key: "id", // using real column name here
-      },
-    },
-    description: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    date: {
-      type: DataTypes.DATEONLY,
-      allowNull: true,
-    },
-    cost: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    currency: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-      validate: {
-        isIn: {
-          args: [Object.values(ExpenseCurrencyTypes)],
-          msg: `Currency must be one of: ${Object.values(ExpenseCurrencyTypes).join(", ")}`,
-        },
-      },
-    },
-    type: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    receiptImage: {
-      type: DataTypes.BLOB,
-      allowNull: true,
-      validate: {
-        hasFileSize(value: Buffer | null) {
-          if (value !== null && this.fileSize === null) {
-            throw new Error("fileSize must be set when receiptImage is set")
-          }
-        },
-      },
-    },
-    fileSize: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      validate: {
-        hasReceiptImage(value: number | null) {
-          if (value !== null && this.receiptImage === null) {
-            throw new Error("receiptImage must be set when fileSize is set")
-          }
-        },
-      },
-    },
-    fileName: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-    },
-    expenseType: {
-      type: DataTypes.STRING(255),
-      allowNull: false,
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-  },
-  {
-    sequelize,
-    modelName: "Expense",
-    tableName: "expenses",
-    paranoid: false,
-    // TODO: consider whether it would be better to use a separate table for uploads
-    // e.g. Rails https://guides.rubyonrails.org/active_storage_overview.html
-    defaultScope: {
-      attributes: { exclude: ["receiptImage"] },
-    },
-    scopes: {
-      withReceiptImage: {
-        attributes: { include: ["receiptImage"] },
-      },
-    },
-  }
-)
 
 export default Expense
