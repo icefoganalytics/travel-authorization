efeab968a79100556aeb948782c87971e359f3e5
Marlen Brunner <klondikemarlen@gmail.com>
2025-07-03 11:31:20 -0700

    :construction: Port DistanceMatrix model over to Sequelize 7 format.

--- api/src/models/distance-matrix.ts
+++ api/src/models/distance-matrix.ts
@@ -1,56 +1,60 @@
 import {
-  CreationOptional,
   DataTypes,
   InferAttributes,
   InferCreationAttributes,
   Model,
+  type CreationOptional,
 } from "@sequelize/core"
+import {
+  Attribute,
+  AutoIncrement,
+  Default,
+  NotNull,
+  PrimaryKey,
+  Table,
+} from "@sequelize/core/decorators-legacy"
 
-import sequelize from "@/db/db-client"
-
+@Table({
+  tableName: "distanceMatrix",
+  timestamps: false,
+  paranoid: false,
+  underscored: false,
+})
 export class DistanceMatrix extends Model<
   InferAttributes<DistanceMatrix>,
   InferCreationAttributes<DistanceMatrix>
 > {
+  @Attribute(DataTypes.INTEGER)
+  @PrimaryKey
+  @AutoIncrement
   declare id: CreationOptional<number>
-  declare origin: string | null
-  declare destination: string | null
-  declare kilometers: number | null
-}
 
-DistanceMatrix.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    // TODO: convert this column to a foreign key to the "locations" table,
-    // or use an external api to get this data.
-    origin: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-    },
-    // TODO: convert this column to a foreign key to the "locations" table,
-    // or use an external api to get this data.
-    destination: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-    },
-    kilometers: {
-      type: DataTypes.FLOAT,
-      allowNull: true,
-    },
-  },
-  {
-    sequelize,
-    modelName: "DistanceMatrix",
-    tableName: "distanceMatrix",
-    underscored: false,
-    timestamps: false,
-    paranoid: false,
+  @Attribute(DataTypes.STRING(255))
+  // TODO: convert this column to a foreign key to the "locations" table,
+  // or use an external api to get this data.
+  declare origin: string | null
+
+  @Attribute(DataTypes.STRING(255))
+  // TODO: convert this column to a foreign key to the "locations" table,
+  // or use an external api to get this data.
+  declare destination: string | null
+
+  @Attribute(DataTypes.FLOAT)
+  declare kilometers: number | null
+
+  @Attribute(DataTypes.DATE)
+  @NotNull
+  @Default(DataTypes.NOW)
+  declare createdAt: CreationOptional<Date>
+
+  @Attribute(DataTypes.DATE)
+  @NotNull
+  @Default(DataTypes.NOW)
+  declare updatedAt: CreationOptional<Date>
+
+  static establishScopes(): void {
+    // add as needed
   }
-)
+}
 
 export default DistanceMatrix
