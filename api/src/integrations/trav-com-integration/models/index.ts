41875ee7c6181f9fec3c8b52665eda1f117d75cb
Marlen Brunner <klondikemarlen@gmail.com>
2025-07-03 18:50:24 -0700

    :construction: Port TravCom Segment model to Sequelize 7 format.

--- api/src/integrations/trav-com-integration/models/index.ts
+++ api/src/integrations/trav-com-integration/models/index.ts
@@ -10,13 +10,18 @@ import AccountsReceivableInvoiceDetail from "./accounts-receivable-invoice-detai
 import City from "./city"
 import Segment from "./segment"
 
-db.addModels([AccountsReceivableInvoice, AccountsReceivableInvoiceDetail, City])
+db.addModels([
+  AccountsReceivableInvoice,
+  AccountsReceivableInvoiceDetail,
+  City,
+  Segment,
+  // add additional models as needed
+])
 
 AccountsReceivableInvoice.establishScopes()
 AccountsReceivableInvoiceDetail.establishScopes()
 City.establishScopes()
-
-Segment.establishAssociations()
+Segment.establishScopes()
 
 export {
   AccountsReceivableInvoice,
--- api/src/integrations/trav-com-integration/models/segment.ts
+++ api/src/integrations/trav-com-integration/models/segment.ts
@@ -1,15 +1,14 @@
 import {
-  Association,
-  CreationOptional,
   DataTypes,
-  ForeignKey,
-  InferAttributes,
-  InferCreationAttributes,
   Model,
-  NonAttribute,
+  type CreationOptional,
+  type InferAttributes,
+  type InferCreationAttributes,
+  type NonAttribute,
 } from "@sequelize/core"
+import { Attribute, BelongsTo, Table } from "@sequelize/core/decorators-legacy"
 
-import sequelize, { MssqlTypeExtensions } from "@/integrations/trav-com-integration/db/db-client"
+import { MssqlTypeExtensions } from "@/integrations/trav-com-integration/db/db-client"
 
 import AccountsReceivableInvoice from "@/integrations/trav-com-integration/models/accounts-receivable-invoice"
 import AccountsReceivableInvoiceDetail from "@/integrations/trav-com-integration/models/accounts-receivable-invoice-detail"
@@ -30,134 +29,140 @@ export type SegmentNoHealthRaw = {
   FareBasis: string | null
 }
 
+@Table({
+  tableName: "segmentsNoHealth",
+  underscored: false,
+  timestamps: false,
+  paranoid: false,
+})
 export class Segment extends Model<InferAttributes<Segment>, InferCreationAttributes<Segment>> {
+  @Attribute({
+    type: DataTypes.DECIMAL(18, 0),
+    field: "segmentID",
+    allowNull: false,
+    primaryKey: true,
+  })
   declare id: CreationOptional<number>
-  declare invoiceId: ForeignKey<AccountsReceivableInvoice["id"]>
-  declare invoiceDetailId: ForeignKey<AccountsReceivableInvoiceDetail["id"]>
+
+  @Attribute({
+    type: DataTypes.DECIMAL(18, 0),
+    field: "invoiceID",
+    allowNull: false,
+  })
+  declare invoiceId: number
+
+  @Attribute({
+    type: DataTypes.DECIMAL(18, 0),
+    field: "invoiceDetailID",
+    allowNull: false,
+  })
+  declare invoiceDetailId: number
+
+  @Attribute({
+    type: DataTypes.TINYINT,
+    field: "LegNumber",
+    allowNull: false,
+  })
   declare legNumber: number
+
+  @Attribute({
+    type: DataTypes.STRING(5),
+    field: "DepartureCityCode",
+    allowNull: true,
+  })
   declare departureCityCode: string | null
+
+  @Attribute({
+    type: MssqlTypeExtensions.DATETIME,
+    field: "DepartureInfo",
+    allowNull: true,
+  })
   declare departureInfo: string | null
+
+  @Attribute({
+    type: DataTypes.STRING(5),
+    field: "ArrivalCityCode",
+    allowNull: true,
+  })
   declare arrivalCityCode: string | null
+
+  @Attribute({
+    type: MssqlTypeExtensions.DATETIME,
+    field: "ArrivalInfo",
+    allowNull: true,
+  })
   declare arrivalInfo: string | null
+
+  @Attribute({
+    type: DataTypes.STRING(3),
+    field: "AirlineCode",
+    allowNull: true,
+  })
   declare airlineCode: string | null
+
+  @Attribute({
+    type: DataTypes.STRING(5),
+    field: "FlightNumber",
+    allowNull: true,
+  })
   declare flightNumber: string | null
+
+  @Attribute({
+    type: DataTypes.STRING(2),
+    field: "ClassOfService",
+    allowNull: true,
+  })
   declare classOfService: string | null
+
+  @Attribute({
+    type: DataTypes.STRING(15),
+    field: "FareBasis",
+    allowNull: true,
+  })
   declare fareBasis: string | null
 
-  // associations
+  // Associations
+  @BelongsTo(() => City, {
+    foreignKey: "arrivalCityCode",
+    targetKey: "cityCode",
+    inverse: {
+      as: "segmentsAsArrivalCity",
+      type: "hasMany",
+    },
+  })
   declare arrivalCity?: NonAttribute<City>
+
+  @BelongsTo(() => City, {
+    foreignKey: "departureCityCode",
+    targetKey: "cityCode",
+    inverse: {
+      as: "segmentsAsDepartureCity",
+      type: "hasMany",
+    },
+  })
   declare departureCity?: NonAttribute<City>
+
+  @BelongsTo(() => AccountsReceivableInvoice, {
+    foreignKey: "invoiceId",
+    inverse: {
+      as: "invoice",
+      type: "hasMany",
+    },
+  })
   declare invoice?: NonAttribute<AccountsReceivableInvoice>
+
+  @BelongsTo(() => AccountsReceivableInvoiceDetail, {
+    foreignKey: "invoiceDetailId",
+    inverse: {
+      as: "segments",
+      type: "hasMany",
+    },
+  })
   declare invoiceDetail?: NonAttribute<AccountsReceivableInvoiceDetail>
 
-  declare static associations: {
-    arrivalCity: Association<Segment, City>
-    departureCity: Association<Segment, City>
-    invoice: Association<Segment, AccountsReceivableInvoice>
-    invoiceDetail: Association<Segment, AccountsReceivableInvoiceDetail>
-  }
-
-  static establishAssociations() {
-    this.belongsTo(AccountsReceivableInvoice, {
-      as: "invoice",
-      foreignKey: "invoiceId",
-    })
-    this.belongsTo(AccountsReceivableInvoiceDetail, {
-      as: "invoiceDetail",
-      foreignKey: "invoiceDetailId",
-    })
-    this.belongsTo(City, {
-      as: "departureCity",
-      foreignKey: "departureCityCode",
-      targetKey: "cityCode",
-    })
-    this.belongsTo(City, {
-      as: "arrivalCity",
-      foreignKey: "arrivalCityCode",
-      targetKey: "cityCode",
-    })
+  static establishScopes(): void {
+    // add as needed
   }
 }
-
-Segment.init(
-  {
-    id: {
-      type: DataTypes.DECIMAL(18, 0),
-      allowNull: false,
-      primaryKey: true,
-      field: "segmentID",
-    },
-    invoiceId: {
-      type: DataTypes.DECIMAL(18, 0),
-      allowNull: false,
-      field: "invoiceID",
-      references: {
-        model: AccountsReceivableInvoice,
-        key: "id",
-      },
-    },
-    invoiceDetailId: {
-      type: DataTypes.DECIMAL(18, 0),
-      allowNull: false,
-      field: "invoiceDetailID",
-      references: {
-        model: AccountsReceivableInvoiceDetail,
-        key: "id",
-      },
-    },
-    legNumber: {
-      type: DataTypes.TINYINT,
-      allowNull: false,
-      field: "LegNumber",
-    },
-    departureCityCode: {
-      type: DataTypes.STRING(5),
-      allowNull: true,
-      field: "DepartureCityCode",
-    },
-    departureInfo: {
-      type: MssqlTypeExtensions.DATETIME,
-      allowNull: true,
-      field: "DepartureInfo",
-    },
-    arrivalCityCode: {
-      type: DataTypes.STRING(5),
-      allowNull: true,
-      field: "ArrivalCityCode",
-    },
-    arrivalInfo: {
-      type: MssqlTypeExtensions.DATETIME,
-      allowNull: true,
-      field: "ArrivalInfo",
-    },
-    airlineCode: {
-      type: DataTypes.STRING(3),
-      allowNull: true,
-      field: "AirlineCode",
-    },
-    flightNumber: {
-      type: DataTypes.STRING(5),
-      allowNull: true,
-      field: "FlightNumber",
-    },
-    classOfService: {
-      type: DataTypes.STRING(2),
-      allowNull: true,
-      field: "ClassOfService",
-    },
-    fareBasis: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      field: "FareBasis",
-    },
-  },
-  {
-    sequelize,
-    tableName: "segmentsNoHealth",
-    underscored: false,
-    timestamps: false,
-    paranoid: false,
-  }
-)
 
 export default Segment
