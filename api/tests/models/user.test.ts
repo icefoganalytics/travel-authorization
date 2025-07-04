383f281edc4403037557f9a5fd9e261dddca86b5
Marlen Brunner <klondikemarlen@gmail.com>
2025-07-04 12:10:16 -0700

    :x: Add failing test showing issue with array columns in Sequelize 7
    See bug report at https://github.com/sequelize/sequelize/issues/15860
    See open patch request at https://github.com/sequelize/sequelize/pull/15994

---
+++ api/tests/models/user.test.ts
@@ -0,0 +1,30 @@
+import { User } from "@/models"
+
+import { userFactory } from "@/factories"
+
+describe("api/src/models/user.ts", () => {
+  describe("User", () => {
+    describe("#roles", async () => {
+      test("when passed an array of strings updates the roles column correctly", async () => {
+        const user = await userFactory.create({
+          roles: [User.Roles.ADMIN],
+        })
+        expect(user.roles).toEqual([User.Roles.ADMIN])
+      })
+    })
+
+    describe(".withScope", () => {
+      describe("#isTravelDeskUser", () => {
+        test("returns users with the travel desk user role", async () => {
+          const user = await userFactory.create({
+            roles: [User.Roles.TRAVEL_DESK_USER],
+          })
+
+          const users = await User.withScope("isTravelDeskUser").findAll()
+
+          expect(users).toEqual([user])
+        })
+      })
+    })
+  })
+})
