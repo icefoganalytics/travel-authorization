2fa007359946e84956ed4f2a7d9818f4f8f536b1
Marlen Brunner <klondikemarlen@gmail.com>
2025-07-16 15:09:29 -0700

    :ok_hand: Make travel time "actuals" completelyable in parallel with expenses.

--- web/src/use/wizards/my-travel-request-wizard-steps.js
+++ web/src/use/wizards/my-travel-request-wizard-steps.js
@@ -159,9 +159,6 @@ export const MY_TRAVEL_REQUEST_WIZARD_STEPS = Object.freeze(
       title: "Submit Expenses",
       subtitle: "Submit trip expenses and receipts to your supervisor",
       component: () => import("@/components/my-travel-request-wizard/SubmitExpensesStep.vue"),
-      backButtonProps: {
-        disabled: true,
-      },
       continueButtonText: "Submit to Supervisor",
       continueButtonProps: {
         disabled: true,
