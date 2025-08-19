29e6292c4cb486e85cab9c7cbb7b6f5a6377d68e
Marlen Brunner <klondikemarlen@gmail.com>
2025-08-19 10:47:52 -0700

    :art: Update add receipt button to typescript.
    I thought it was broken, but it was just my browser needing to restart.

--- web/src/components/expenses/edit-data-table/AddReceiptButton.vue
+++ web/src/components/expenses/edit-data-table/AddReceiptButton.vue
@@ -1,10 +1,10 @@
 <template>
   <span class="ml-2">
     <input
-      ref="fileInput"
+      ref="fileInputRef"
       class="d-none"
       type="file"
-      @change="onFileSelected"
+      @change="uploadFileAndEmit"
     />
     <v-btn
       color="primary"
@@ -16,43 +16,49 @@
   </span>
 </template>
 
-<script>
-import expensesApi from "@/api/expenses-api"
+<script setup lang="ts">
+import { isEmpty, isNil } from "lodash"
+import { ref } from "vue"
 
-export default {
-  props: {
-    expenseId: {
-      type: Number,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      file: null,
-      isLoading: false,
-    }
-  },
-  methods: {
-    triggerFileInput() {
-      this.$refs.fileInput.click()
-    },
-    onFileSelected(event) {
-      this.file = event.target.files[0]
-      this.uploadFile()
-    },
-    async uploadFile() {
-      this.isLoading = true
-      try {
-        await expensesApi.upload(this.expenseId, this.file)
-        this.$emit("uploaded")
-        this.$snack("Receipt added", { color: "success" })
-      } catch (error) {
-        console.error(error)
-        this.$snack(`Failed to add receipt: ${error}`, { color: "error" })
-      } finally {
-        this.isLoading = false
-      }
-    },
-  },
+import expensesApi from "@/api/expenses-api"
+import useSnack from "@/use/use-snack"
+
+const props = defineProps<{
+  expenseId: number
+}>()
+
+// TODO: switch to `uploaded: [void]` syntax in vue 3
+const emit = defineEmits<{
+  (event: "uploaded"): void
+}>()
+
+const fileInputRef = ref<HTMLInputElement | null>(null)
+const isLoading = ref(false)
+
+function triggerFileInput() {
+  fileInputRef.value?.click()
+}
+
+const snack = useSnack()
+
+async function uploadFileAndEmit(event: Event) {
+  const target = event.target as HTMLInputElement
+  const { files } = target
+  if (isNil(files) || isEmpty(files)) return
+
+  const file = files[0]
+
+  isLoading.value = true
+  try {
+    await expensesApi.upload(props.expenseId, file)
+
+    emit("uploaded")
+    snack.success("Receipt uploaded!")
+  } catch (error) {
+    console.error(`Failed to upload receipt: ${error}`, { error })
+    snack.error(`Failed to upload receipt: ${error}`)
+  } finally {
+    isLoading.value = false
+  }
 }
 </script>
