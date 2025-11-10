f002350d1bb9ec4421106de0d57cc1f98907ef66
Marlen Brunner <klondikemarlen@gmail.com>
2025-11-10 15:10:04 -0700

    :bug: Re-add datetime type extension handling of string and number values.
    Why? Apparently while the value is normally Date type, if you have a scope like
    this.addScope("invoiceBookingDateBetween", ([date1, date2]: [string, string]) => {
      const [startDate, endDate] = sortBy([date1, date2])
    
      return {
        include: {
          association: "invoice",
          where: {
            bookingDate: {
              [Op.gte]: startDate,
              [Op.lte]: endDate,
            },
          },
        },
      }
    })
    
    Then this will send string values, that must be formatted to an MSSQL DATETIME friendly format.

--- api/src/integrations/trav-com-integration/db/mssql-type-extensions/datetime.ts
+++ api/src/integrations/trav-com-integration/db/mssql-type-extensions/datetime.ts
@@ -9,8 +9,18 @@ export class DATETIME extends DataTypes.ABSTRACT<Date> {
     return "DATETIME"
   }
 
-  toBindableValue(value: Date): string {
-    return DateTime.fromJSDate(value).toFormat("yyyy-MM-dd HH:mm:ss")
+  toBindableValue(value: Date | string | number): string {
+    if (typeof value === "string") {
+      return DateTime.fromISO(value).toFormat("yyyy-MM-dd HH:mm:ss")
+    } else if (typeof value === "number") {
+      return DateTime.fromMillis(value).toFormat("yyyy-MM-dd HH:mm:ss")
+    } else if (value instanceof Date) {
+      return DateTime.fromJSDate(value).toFormat("yyyy-MM-dd HH:mm:ss")
+    }
+
+    throw new Error(
+      `Unsupported value type: ${value} -> ${typeof value} -> ${JSON.stringify(value)}`
+    )
   }
 }
 
