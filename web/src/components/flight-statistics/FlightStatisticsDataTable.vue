0c3e65bd2d8f6e33d8b3972373ca8c87fac7b342
Marlen Brunner <klondikemarlen@gmail.com>
2025-11-12 17:35:36 -0700

    :construction: Begin cleaning up FlightStatisticsDataTable.
    Lots more to go.

--- web/src/components/flight-statistics/FlightStatisticsDataTable.vue
+++ web/src/components/flight-statistics/FlightStatisticsDataTable.vue
@@ -1,29 +1,26 @@
 <template>
   <div class="mx-10 mb-5">
-    <v-row
-      v-if="admin && !loadingData"
-      class="my-0 mx-0"
-    >
-      <v-btn
-        class="ml-auto"
-        color="secondary"
-        @click="exportToExcel()"
-      >
-        Download Data
-      </v-btn>
-
-      <print-report
-        :flight-report="flightReport"
-        @close="printClosed()"
-      />
-    </v-row>
-
     <v-data-table
       :headers="headers"
       :items="flightReport"
       :items-per-page="15"
       class="elevation-1 mt-4"
     >
+      <template
+        v-if="isAdmin"
+        #top
+      >
+        <v-btn
+          class="ml-auto"
+          color="secondary"
+          @click="exportToExcel"
+        >
+          Download Data
+        </v-btn>
+
+        <PrintReport :flight-report="flightReport" />
+      </template>
+
       <template #item.totalExpenses="{ item }">
         <span v-if="item.totalExpenses > 0"
           >${{ Number(item.totalExpenses).toFixed(2) | currency }}</span
@@ -48,137 +45,121 @@
   </div>
 </template>
 
-<script>
-import Vue from "vue"
+<script setup lang="ts">
+import { ref } from "vue"
 import { ExportToCsv } from "export-to-csv"
+
+import { type FlightStatisticAsIndex } from "@/api/flight-statistics-api"
+import useCurrentUser from "@/use/use-current-user"
+
 import PrintReport from "@/modules/reports/views/Common/PrintReport.vue"
 
-export default {
-  name: "FlightStatisticsDataTable",
-  components: {
-    PrintReport,
+const props = withDefaults(
+  defineProps<{
+    flightReport: FlightStatisticAsIndex[]
+  }>(),
+  {
+    flightReport: () => [],
+  }
+)
+
+const { isAdmin } = useCurrentUser<true>()
+
+const headers = [
+  {
+    text: "Department",
+    value: "department",
+    class: "blue-grey lighten-4",
   },
-  props: {
-    flightReport: {
-      type: Array,
-      default: () => [],
-    },
+  {
+    text: "Final Destination City",
+    value: "destinationCity",
+    class: "blue-grey lighten-4",
   },
-  data() {
+  {
+    text: "Final Destination Province",
+    value: "destinationProvince",
+    class: "blue-grey lighten-4",
+  },
+  {
+    text: "Total Trips",
+    value: "totalTrips",
+    class: "blue-grey lighten-4",
+  },
+  {
+    text: "Total Expenses",
+    value: "totalExpenses",
+    class: "blue-grey lighten-4",
+  },
+  {
+    text: "Total Flight Cost",
+    value: "totalFlightCost",
+    class: "blue-grey lighten-4",
+  },
+  {
+    text: "Average Duration (days)",
+    value: "averageDurationDays",
+    class: "blue-grey lighten-4",
+  },
+  {
+    text: "Average Expenses per Day",
+    value: "averageExpensesPerDay",
+    class: "blue-grey lighten-4",
+  },
+  {
+    text: "Average Round Trip Flight Cost",
+    value: "averageRoundTripFlightCost",
+    class: "blue-grey lighten-4",
+  },
+]
+
+const loadingData = ref(false)
+
+async function exportToExcel() {
+  const csvInfo = props.flightReport.map((flight) => {
     return {
-      headers: [
-        {
-          text: "Department",
-          value: "department",
-          class: "blue-grey lighten-4",
-        },
-        {
-          text: "Final Destination City",
-          value: "destinationCity",
-          class: "blue-grey lighten-4",
-        },
-        {
-          text: "Final Destination Province",
-          value: "destinationProvince",
-          class: "blue-grey lighten-4",
-        },
-        {
-          text: "Total Trips",
-          value: "totalTrips",
-          class: "blue-grey lighten-4",
-        },
-        {
-          text: "Total Expenses",
-          value: "totalExpenses",
-          class: "blue-grey lighten-4",
-        },
-        {
-          text: "Total Flight Cost",
-          value: "totalFlightCost",
-          class: "blue-grey lighten-4",
-        },
-        {
-          text: "Average Duration (days)",
-          value: "averageDurationDays",
-          class: "blue-grey lighten-4",
-        },
-        {
-          text: "Average Expenses per Day",
-          value: "averageExpensesPerDay",
-          class: "blue-grey lighten-4",
-        },
-        {
-          text: "Average Round Trip Flight Cost",
-          value: "averageRoundTripFlightCost",
-          class: "blue-grey lighten-4",
-        },
-      ],
-      admin: false,
-      flightReportInfo: [],
-      loadingData: false,
+      department: flight.department ? flight.department : "",
+      finalDestinationCity: flight.destinationCity ? flight.destinationCity : "",
+      finalDestinationProvince: flight.destinationProvince ? flight.destinationProvince : "",
+      totalTrips: flight.totalTrips ? flight.totalTrips : "",
+      totalExpenses: flight.totalExpenses ? "$ " + Number(flight.totalExpenses).toFixed(2) : "",
+      totalFlightCost: flight.totalFlightCost
+        ? "$ " + Number(flight.totalFlightCost).toFixed(2)
+        : "",
+      averageDurationDays: flight.averageDurationDays ? flight.averageDurationDays : "",
+      averageExpensesPerDay: flight.averageExpensesPerDay
+        ? "$ " + Number(flight.averageExpensesPerDay).toFixed(2)
+        : "",
+      averageRoundTripFlightCost: flight.averageRoundTripFlightCost
+        ? "$ " + Number(flight.averageRoundTripFlightCost).toFixed(2)
+        : "",
     }
-  },
-  mounted() {
-    this.admin = true
-    //TODO: update this
-    const temp = Vue.filter("isAdmin")()
-    console.log(temp)
-    // this.flightReportInfo = this.flightReport;
-  },
-
-  methods: {
-    exportToExcel() {
-      const csvInfo = this.flightReport.map((flight) => {
-        return {
-          department: flight.department ? flight.department : "",
-          finalDestinationCity: flight.destinationCity ? flight.destinationCity : "",
-          finalDestinationProvince: flight.destinationProvince ? flight.destinationProvince : "",
-          totalTrips: flight.totalTrips ? flight.totalTrips : "",
-          totalExpenses: flight.totalExpenses ? "$ " + Number(flight.totalExpenses).toFixed(2) : "",
-          totalFlightCost: flight.totalFlightCost
-            ? "$ " + Number(flight.totalFlightCost).toFixed(2)
-            : "",
-          averageDurationDays: flight.averageDurationDays ? flight.averageDurationDays : "",
-          averageExpensesPerDay: flight.averageExpensesPerDay
-            ? "$ " + Number(flight.averageExpensesPerDay).toFixed(2)
-            : "",
-          averageRoundTripFlightCost: flight.averageRoundTripFlightCost
-            ? "$ " + Number(flight.averageRoundTripFlightCost).toFixed(2)
-            : "",
-        }
-      })
-      const options = {
-        fieldSeparator: ",",
-        quoteStrings: '"',
-        decimalSeparator: ".",
-        showLabels: true,
-        showTitle: false,
-        title: "",
-        filename: "Flights",
-        useTextFile: false,
-        useBom: true,
-        useKeysAsHeaders: false,
-        headers: [
-          "Department",
-          "Final Destination City",
-          "Final Destination Province",
-          "Total Trips",
-          "Total Expenses",
-          "Total Flight Cost",
-          "Average Duration (days)",
-          "Average Expenses per Day",
-          "Average Round Trip Flight Cost",
-        ],
-      }
-      const csvExporter = new ExportToCsv(options)
-      csvExporter.generateCsv(csvInfo)
-    },
-
-    printClosed() {
-      this.loadingData = true
-      Vue.nextTick(() => (this.loadingData = false))
-    },
-  },
+  })
+  const options = {
+    fieldSeparator: ",",
+    quoteStrings: '"',
+    decimalSeparator: ".",
+    showLabels: true,
+    showTitle: false,
+    title: "",
+    filename: "Flights",
+    useTextFile: false,
+    useBom: true,
+    useKeysAsHeaders: false,
+    headers: [
+      "Department",
+      "Final Destination City",
+      "Final Destination Province",
+      "Total Trips",
+      "Total Expenses",
+      "Total Flight Cost",
+      "Average Duration (days)",
+      "Average Expenses per Day",
+      "Average Round Trip Flight Cost",
+    ],
+  }
+  const csvExporter = new ExportToCsv(options)
+  csvExporter.generateCsv(csvInfo)
 }
 </script>
 
