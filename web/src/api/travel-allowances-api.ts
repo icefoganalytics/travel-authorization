cb959d43fba5b6df0ab66b5b17cc0c2449b91522
Marlen Brunner <klondikemarlen@gmail.com>
2025-12-12 15:45:58 -0700

    :broom: Fix some travel allowance type conversions.

--- web/src/api/travel-allowances-api.ts
+++ web/src/api/travel-allowances-api.ts
@@ -6,7 +6,15 @@ import {
   type WhereOptions,
 } from "@/api/base-api"
 
-/** @deprecated - prefer enum equivalent `AllowanceTypes` */
+/** Keep in sync with api/src/models/travel-allowance.ts */
+export enum TravelAllowanceAllowanceTypes {
+  MAXIUM_AIRCRAFT_ALLOWANCE = "maxium_aircraft_allowance",
+  AIRCRAFT_ALLOWANCE_PER_SEGMENT = "aircraft_allowance_per_segment",
+  DISTANCE_ALLOWANCE_PER_KILOMETER = "distance_allowance_per_kilometer",
+  HOTEL_ALLOWANCE_PER_NIGHT = "hotel_allowance_per_night",
+}
+
+/** @deprecated - prefer enum equivalent `TravelAllowanceAllowanceTypes` */
 export const TRAVEL_ALLOWANCE_ALLOWANCE_TYPES = Object.freeze({
   MAXIUM_AIRCRAFT_ALLOWANCE: "maxium_aircraft_allowance",
   AIRCRAFT_ALLOWANCE_PER_SEGMENT: "aircraft_allowance_per_segment",
@@ -14,31 +22,23 @@ export const TRAVEL_ALLOWANCE_ALLOWANCE_TYPES = Object.freeze({
   HOTEL_ALLOWANCE_PER_NIGHT: "hotel_allowance_per_night",
 })
 
-/** @deprecated - prefer enum equivalent `CurrencyTypes` */
+/** Keep in sync with api/src/models/travel-allowance.ts */
+export enum TravelAllowanceCurrencyTypes {
+  USD = "USD",
+  CAD = "CAD",
+}
+
+/** @deprecated - prefer enum equivalent `TravelAllowanceCurrencyTypes` */
 export const TRAVEL_ALLOWANCE_CURRENCY_TYPES = Object.freeze({
   USD: "USD",
   CAD: "CAD",
 })
 
-/** Keep in sync with api/src/models/travel-allowance.ts */
-export enum AllowanceTypes {
-  MAXIUM_AIRCRAFT_ALLOWANCE = "maxium_aircraft_allowance",
-  AIRCRAFT_ALLOWANCE_PER_SEGMENT = "aircraft_allowance_per_segment",
-  DISTANCE_ALLOWANCE_PER_KILOMETER = "distance_allowance_per_kilometer",
-  HOTEL_ALLOWANCE_PER_NIGHT = "hotel_allowance_per_night",
-}
-
-/** Keep in sync with api/src/models/travel-allowance.ts */
-export enum CurrencyTypes {
-  USD = "USD",
-  CAD = "CAD",
-}
-
 export type TravelAllowance = {
   id: string
-  allowanceType: AllowanceTypes
+  allowanceType: TravelAllowanceAllowanceTypes
   amount: number
-  currency: CurrencyTypes
+  currency: TravelAllowanceCurrencyTypes
   createdAt: string
   updatedAt: string
 }
@@ -80,6 +80,7 @@ export const travelAllowancesApi = {
     attributes: Partial<TravelAllowance>
   ): Promise<{
     travelAllowance: TravelAllowance
+    policy: Policy
   }> {
     const { data } = await http.patch(`/api/travel-allowances/${travelAllowanceId}`, attributes)
     return data
