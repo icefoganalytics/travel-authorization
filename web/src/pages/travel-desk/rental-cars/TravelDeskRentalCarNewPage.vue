ff12cd02ed8e483a1de07224ca37bfc5273d6e79
Marlen Brunner <klondikemarlen@gmail.com>
2026-01-13 10:46:14 -0700

    :cherry_blossom: use nicer UI layout for rental car request creation.
    Thank Google for https://stitch.withgoogle.com/projects/16327873998777013500?pli=1.

--- web/src/pages/travel-desk/rental-cars/TravelDeskRentalCarNewPage.vue
+++ web/src/pages/travel-desk/rental-cars/TravelDeskRentalCarNewPage.vue
@@ -9,197 +9,199 @@
     <v-row>
       <v-col
         cols="12"
-        md="4"
+        md="6"
       >
-        <LocationsAutocomplete
-          v-model="attributes.pickUpCity"
-          label="Pick-Up City *"
-          :rules="[required]"
-          item-value="city"
-          outlined
-          required
-        />
-      </v-col>
-      <v-col
-        cols="12"
-        md="4"
-      >
-        <TravelDeskRentalCarLocationTypeSelect
-          v-model="attributes.pickUpLocation"
-          label="Pick-Up Location *"
-          :rules="[required]"
-          outlined
-          required
-          @input="resetPickUpLocationOtherUnlessOther"
-        />
-      </v-col>
-      <v-col
-        cols="12"
-        md="4"
-      >
-        <TravelDeskRentalCarVehicleTypeSelect
-          v-model="attributes.vehicleType"
-          label="Vehicle Type *"
-          :rules="[required]"
-          outlined
-          required
-          @input="resetVehicleChangeRationaleIfCompact"
-        />
-      </v-col>
-    </v-row>
+        <v-card-title>1. Trip Schedule</v-card-title>
+        <v-row>
+          <v-col cols="6">
+            <v-text-field
+              v-model="pickUpDate"
+              label="Pick-up date *"
+              type="date"
+              :disabled="travelDeskRentalCarAttributes.matchFlightTimes"
+              :min="tripStartDate"
+              :max="tripEndDate"
+              :rules="[required]"
+              outlined
+              required
+            />
+          </v-col>
+          <v-col cols="6">
+            <v-text-field
+              v-model="pickUpTime"
+              label="Pick-up time *"
+              type="time"
+              :rules="[required]"
+              outlined
+              required
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="6">
+            <v-text-field
+              v-model="dropOffDate"
+              label="Drop-off date *"
+              type="date"
+              :disabled="travelDeskRentalCarAttributes.matchFlightTimes"
+              :min="tripStartDate"
+              :max="tripEndDate"
+              :rules="[required]"
+              outlined
+              required
+            />
+          </v-col>
+          <v-col cols="6">
+            <v-text-field
+              v-model="dropOffTime"
+              label="Drop-off time *"
+              type="time"
+              :rules="[required]"
+              outlined
+              required
+            />
+          </v-col>
+        </v-row>
 
-    <v-row v-if="attributes.pickUpLocation === TravelDeskRentalCarLocationTypes.OTHER">
-      <v-col cols="12">
-        <v-text-field
-          v-model="attributes.pickUpLocationOther"
-          label="Other Pick-Up Location *"
-          :rules="[required]"
-          outlined
-          required
-        />
+        <v-row>
+          <v-col>
+            <YesNoRowRadioGroup
+              v-model="travelDeskRentalCarAttributes.matchFlightTimes"
+              label="Pick-up/Drop-off match flights"
+              @change="matchWithFlight"
+            />
+          </v-col>
+        </v-row>
+        <v-divider />
+        <v-card-title>2. Location Details</v-card-title>
+        <v-row>
+          <v-col cols="12">
+            <LocationsAutocomplete
+              v-model="travelDeskRentalCarAttributes.pickUpCity"
+              label="Pick-up City *"
+              :rules="[required]"
+              item-value="city"
+              outlined
+              required
+            />
+          </v-col>
+          <v-col cols="12">
+            <TravelDeskRentalCarLocationTypeSelect
+              v-model="travelDeskRentalCarAttributes.pickUpLocation"
+              label="Pick-up Location *"
+              :rules="[required]"
+              outlined
+              required
+              @input="resetPickUpLocationOtherUnlessOther"
+            />
+            <v-text-field
+              v-if="
+                travelDeskRentalCarAttributes.pickUpLocation ===
+                TravelDeskRentalCarLocationTypes.OTHER
+              "
+              v-model="travelDeskRentalCarAttributes.pickUpLocationOther"
+              label="Other Pick-up Location *"
+              :rules="[required]"
+              outlined
+              required
+            />
+          </v-col>
+        </v-row>
+        <v-divider />
+        <v-row>
+          <v-col>
+            <YesNoRowRadioGroup
+              v-model="travelDeskRentalCarAttributes.sameDropOffLocation"
+              label="Same Drop-off location?"
+              class="mt-1"
+              @change="resetDropOffLocationStates"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <LocationsAutocomplete
+              v-if="travelDeskRentalCarAttributes.sameDropOffLocation === false"
+              v-model="travelDeskRentalCarAttributes.dropOffCity"
+              label="Drop-off City *"
+              :rules="[required]"
+              item-value="city"
+              outlined
+              required
+            />
+          </v-col>
+          <v-col cols="12">
+            <TravelDeskRentalCarLocationTypeSelect
+              v-if="travelDeskRentalCarAttributes.sameDropOffLocation === false"
+              v-model="travelDeskRentalCarAttributes.dropOffLocation"
+              label="Drop-off Location *"
+              :rules="[required]"
+              outlined
+              required
+              @input="updateDropOffLocation"
+            />
+            <v-text-field
+              v-if="
+                travelDeskRentalCarAttributes.sameDropOffLocation === false &&
+                travelDeskRentalCarAttributes.dropOffLocation ===
+                  TravelDeskRentalCarLocationTypes.OTHER
+              "
+              v-model="travelDeskRentalCarAttributes.dropOffLocationOther"
+              label="Other Drop-off Location *"
+              class="mt-n3"
+              :rules="[required]"
+              outlined
+              required
+            />
+          </v-col>
+        </v-row>
       </v-col>
-    </v-row>
-
-    <v-row>
       <v-col
         cols="12"
         md="6"
       >
-        <YesNoRowRadioGroup
-          v-model="attributes.sameDropOffLocation"
-          label="Same Drop-off Location?"
-          @change="resetDropOffLocationStates"
-        />
-      </v-col>
-      <v-col
-        cols="12"
-        md="6"
-      >
-        <YesNoRowRadioGroup
-          v-model="attributes.matchFlightTimes"
-          label="Match Flight Times?"
-        />
-      </v-col>
-    </v-row>
-
-    <v-row v-if="attributes.sameDropOffLocation === false">
-      <v-col
-        cols="12"
-        md="6"
-      >
-        <LocationsAutocomplete
-          v-model="attributes.dropOffCity"
-          label="Drop-off City *"
-          :rules="[required]"
-          item-value="city"
-          outlined
-          required
-        />
-      </v-col>
-      <v-col
-        cols="12"
-        md="6"
-      >
-        <TravelDeskRentalCarLocationTypeSelect
-          :value="attributes.dropOffLocation"
-          label="Drop-off Location *"
-          :rules="[required]"
-          outlined
-          required
-          @input="updateDropOffLocation"
-        />
+        <v-card-title>3. Vehicle & Extra Info</v-card-title>
+        <v-row>
+          <v-col cols="12">
+            <TravelDeskRentalCarVehicleTypeSelect
+              v-model="travelDeskRentalCarAttributes.vehicleType"
+              label="Vehicle Type *"
+              :rules="[required]"
+              outlined
+              required
+              @input="resetVehicleChangeRationaleIfCompact"
+            />
+          </v-col>
+          <v-col cols="12">
+            <v-textarea
+              v-if="
+                travelDeskRentalCarAttributes.vehicleType !==
+                TravelDeskRentalCarVehicleTypes.COMPACT
+              "
+              v-model="travelDeskRentalCarAttributes.vehicleChangeRationale"
+              label="Reason for Change *"
+              hint="Please provide a reason for requesting a vehicle type other than Compact."
+              :rules="[required]"
+              outlined
+              persistent-hint
+              required
+              rows="4"
+            />
+          </v-col>
+          <v-col cols="12">
+            <v-textarea
+              v-model="travelDeskRentalCarAttributes.additionalNotes"
+              label="Additional Information"
+              outlined
+              rows="4"
+              clearable
+            />
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
 
-    <v-row
-      v-if="
-        attributes.sameDropOffLocation === false &&
-        attributes.dropOffLocation === TravelDeskRentalCarLocationTypes.OTHER
-      "
-    >
-      <v-col cols="12">
-        <v-text-field
-          v-model="attributes.dropOffLocationOther"
-          label="Other Drop-off Location *"
-          :rules="[required]"
-          outlined
-          required
-        />
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-col
-        cols="12"
-        md="3"
-      >
-        <v-text-field
-          v-model="pickUpDate"
-          label="Pick-Up Date *"
-          type="date"
-          :rules="[required]"
-          outlined
-          required
-        />
-      </v-col>
-      <v-col
-        cols="12"
-        md="3"
-      >
-        <TimeTextField
-          v-model="pickUpTime"
-          label="Pick-Up Time *"
-          :rules="[required]"
-        />
-      </v-col>
-      <v-col
-        cols="12"
-        md="3"
-      >
-        <v-text-field
-          v-model="dropOffDate"
-          label="Drop-off Date *"
-          type="date"
-          :rules="[required]"
-          outlined
-          required
-        />
-      </v-col>
-      <v-col
-        cols="12"
-        md="3"
-      >
-        <TimeTextField
-          v-model="dropOffTime"
-          label="Drop-off Time *"
-          :rules="[required]"
-        />
-      </v-col>
-    </v-row>
-
-    <v-row v-if="attributes.vehicleType !== TravelDeskRentalCarVehicleTypes.COMPACT">
-      <v-col cols="12">
-        <v-textarea
-          v-model="attributes.vehicleChangeRationale"
-          label="Reason for Vehicle Change *"
-          :rules="[required]"
-          outlined
-          rows="3"
-          required
-        />
-      </v-col>
-    </v-row>
-
-    <v-row>
-      <v-col cols="12">
-        <v-textarea
-          v-model="attributes.additionalNotes"
-          label="Additional Notes"
-          outlined
-          rows="3"
-        />
-      </v-col>
-    </v-row>
+    <v-divider />
 
     <template #actions>
       <v-btn
@@ -234,9 +236,9 @@ import travelDeskRentalCarsApi, {
 
 import useBreadcrumbs from "@/use/use-breadcrumbs"
 import useSnack from "@/use/use-snack"
+import useTravelTimesSummary from "@/use/travel-desk-travel-requests/use-travel-times-summary"
 
 import HeaderActionsFormCard from "@/components/common/HeaderActionsFormCard.vue"
-import TimeTextField from "@/components/common/TimeTextField.vue"
 import YesNoRowRadioGroup from "@/components/common/YesNoRowRadioGroup.vue"
 
 import TravelDeskRentalCarLocationTypeSelect from "@/components/travel-request-rental-cars/TravelDeskRentalCarLocationTypeSelect.vue"
@@ -250,7 +252,11 @@ const props = defineProps<{
 
 const travelDeskTravelRequestIdAsNumber = computed(() => parseInt(props.travelDeskTravelRequestId))
 
-const attributes = ref<Partial<TravelDeskRentalCar>>({
+const { tripStartDate, tripEndDate, flightStartDate, flightEndDate } = useTravelTimesSummary(
+  travelDeskTravelRequestIdAsNumber
+)
+
+const travelDeskRentalCarAttributes = ref<Partial<TravelDeskRentalCar>>({
   travelRequestId: travelDeskTravelRequestIdAsNumber.value,
   pickUpCity: undefined,
   pickUpLocation: undefined,
@@ -272,28 +278,37 @@ const dropOffTime = ref("12:00")
 
 function resetPickUpLocationOtherUnlessOther(value: string) {
   if (value !== TravelDeskRentalCarLocationTypes.OTHER) {
-    attributes.value.pickUpLocationOther = undefined
+    travelDeskRentalCarAttributes.value.pickUpLocationOther = undefined
   }
 }
 
 function updateDropOffLocation(value: string) {
-  attributes.value.dropOffLocation = value
+  travelDeskRentalCarAttributes.value.dropOffLocation = value
   if (value !== TravelDeskRentalCarLocationTypes.OTHER) {
-    attributes.value.dropOffLocationOther = undefined
+    travelDeskRentalCarAttributes.value.dropOffLocationOther = undefined
   }
 }
 
 function resetDropOffLocationStates(value: boolean) {
   if (value === true) {
-    attributes.value.dropOffCity = undefined
-    attributes.value.dropOffLocation = undefined
-    attributes.value.dropOffLocationOther = undefined
+    travelDeskRentalCarAttributes.value.dropOffCity = undefined
+    travelDeskRentalCarAttributes.value.dropOffLocation = undefined
+    travelDeskRentalCarAttributes.value.dropOffLocationOther = undefined
   }
 }
 
 function resetVehicleChangeRationaleIfCompact(value: string) {
   if (value === TravelDeskRentalCarVehicleTypes.COMPACT) {
-    attributes.value.vehicleChangeRationale = undefined
+    travelDeskRentalCarAttributes.value.vehicleChangeRationale = undefined
+  }
+}
+
+function matchWithFlight(value: boolean) {
+  if (value && flightStartDate.value && flightEndDate.value) {
+    pickUpDate.value = flightStartDate.value
+    dropOffDate.value = flightEndDate.value
+    pickUpTime.value = "12:00"
+    dropOffTime.value = "12:00"
   }
 }
 
@@ -305,12 +320,12 @@ const router = useRouter()
 async function createAndReturn() {
   if (!headerActionsFormCard.value?.validate()) return
 
-  attributes.value.pickUpDate = `${pickUpDate.value}T${pickUpTime.value}:00.000Z`
-  attributes.value.dropOffDate = `${dropOffDate.value}T${dropOffTime.value}:00.000Z`
+  travelDeskRentalCarAttributes.value.pickUpDate = `${pickUpDate.value}T${pickUpTime.value}:00.000Z`
+  travelDeskRentalCarAttributes.value.dropOffDate = `${dropOffDate.value}T${dropOffTime.value}:00.000Z`
 
   isSaving.value = true
   try {
-    await travelDeskRentalCarsApi.create(attributes.value)
+    await travelDeskRentalCarsApi.create(travelDeskRentalCarAttributes.value)
     snack.success("Rental car created successfully")
 
     return router.push({
