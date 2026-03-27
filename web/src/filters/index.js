3f0b32dd799eeb93cb86ecb5ca5aa9344c753f5d
Marlen Brunner <klondikemarlen@gmail.com>
2026-03-27 08:52:13 -0700

    :recycle: Remove unused legacy isTdUser Vue filter.

--- web/src/filters/index.js
+++ web/src/filters/index.js
@@ -2,13 +2,6 @@ import Vue from "vue"
 
 import store from "@/store"
 
-Vue.filter("isTdUser", function () {
-  const userRoles = store.state.auth?.user?.roles
-  const admin = userRoles?.includes("admin")
-  const TdUser = userRoles?.includes("travel_desk_user")
-  return admin || TdUser
-})
-
 Vue.filter("flightStartEnd", function (flights) {
   if (flights.length > 0) {
     const dates = flights.map((flight) => flight.datePreference)
