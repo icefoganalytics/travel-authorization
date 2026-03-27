c768f233adc099b041747b1dedf986299ea09e59
Marlen Brunner <klondikemarlen@gmail.com>
2026-03-27 15:06:32 -0700

    :recycle: Move health check page to typed API module.

--- web/src/pages/HealthCheckPage.vue
+++ web/src/pages/HealthCheckPage.vue
@@ -1,7 +1,12 @@
 <template>
   <v-app>
     <div>
-      <router-link :to="{ name: 'DashboardPage' }">Dashboard</router-link>
+      <router-link
+        :to="{
+          name: 'DashboardPage',
+        }"
+        >Dashboard</router-link
+      >
 
       <v-row class="mt-5">
         <v-col
@@ -111,57 +116,41 @@
   </v-app>
 </template>
 
-<script>
-import http from "@/api/http-client"
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue"
 
-import { RELEASE_TAG, GIT_COMMIT_HASH } from "@/config"
+import { GIT_COMMIT_HASH, RELEASE_TAG } from "@/config"
+
+import { healthCheckApi, type HealthCheck } from "@/api/health-check-api"
 
 import useSnack from "@/use/use-snack"
 
-export default {
-  name: "HealthCheckPage",
-  components: {},
-  setup() {
-    const snack = useSnack()
+const healthCheck = ref<HealthCheck>({
+  appHealth: {},
+  dbHealth: {},
+})
 
-    return {
-      snack,
-    }
-  },
-  data: () => ({
-    healthCheck: {
-      appHealth: {},
-      dbHealth: {},
-    },
-    // TODO: load from back-end
-    environment: {
-      releaseTag: RELEASE_TAG,
-      gitCommitHash: GIT_COMMIT_HASH,
-    },
-  }),
-  computed: {
-    dbHealth() {
-      return this.healthCheck.dbHealth || {}
-    },
-    appHealth() {
-      return this.healthCheck.appHealth || {}
-    },
-  },
-  async mounted() {
-    await this.refresh()
-  },
-  methods: {
-    refresh() {
-      return http
-        .get("/api/health-check")
-        .then(({ data }) => {
-          this.healthCheck = data
-        })
-        .catch((error) => {
-          console.error(`Failed to fetch health check data: ${error}`, { error })
-          this.snack.error(`Failed to fetch health check data: ${error}`)
-        })
-    },
-  },
+const environment = {
+  releaseTag: RELEASE_TAG,
+  gitCommitHash: GIT_COMMIT_HASH,
 }
+
+const appHealth = computed(() => healthCheck.value.appHealth || {})
+const dbHealth = computed(() => healthCheck.value.dbHealth || {})
+
+const snack = useSnack()
+
+async function refresh() {
+  try {
+    const { healthCheck: healthCheckUpdate } = await healthCheckApi.get()
+    healthCheck.value = healthCheckUpdate
+  } catch (error) {
+    console.error(`Failed to fetch health check data: ${error}`, { error })
+    snack.error(`Failed to fetch health check data: ${error}`)
+  }
+}
+
+onMounted(async () => {
+  await refresh()
+})
 </script>
