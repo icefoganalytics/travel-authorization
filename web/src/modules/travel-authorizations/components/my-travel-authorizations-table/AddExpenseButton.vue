f77fda25221001e10052f70efa1e9c06c761a2cf
Marlen Brunner <klondikemarlen@gmail.com>
2026-03-27 11:19:24 -0700

    :recycle: Convert AddExpenseButton to script setup.

--- web/src/modules/travel-authorizations/components/my-travel-authorizations-table/AddExpenseButton.vue
+++ web/src/modules/travel-authorizations/components/my-travel-authorizations-table/AddExpenseButton.vue
@@ -8,19 +8,12 @@
   </v-btn>
 </template>
 
-<script>
-export default {
-  name: "AddExpenseButton",
-  props: {
-    travelAuthorizationId: {
-      type: Number,
-      required: true,
-    },
-  },
-  methods: {
-    addExpense() {
-      alert("TODO: add expense for travel authorization " + this.travelAuthorizationId)
-    },
-  },
+<script setup lang="ts">
+const props = defineProps<{
+  travelAuthorizationId: number
+}>()
+
+function addExpense() {
+  alert(`TODO: add expense for travel authorization ${props.travelAuthorizationId}`)
 }
 </script>
