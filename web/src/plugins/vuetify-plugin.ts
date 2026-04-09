e227553a2cc301e2abbb4785c5a7cc9332efabc2
Marlen Brunner <klondikemarlen@gmail.com>
2026-04-09 15:10:53 -0700

    :art: Update Vuetify theme to Yukon North Star with official brand palette

--- web/src/plugins/vuetify-plugin.ts
+++ web/src/plugins/vuetify-plugin.ts
@@ -7,13 +7,18 @@ import "@/assets/vuetify2-extensions.css"
 
 export default createVuetify({
   theme: {
-    defaultTheme: "light",
+    defaultTheme: "yukonNorthStar",
     themes: {
-      light: {
+      yukonNorthStar: {
         colors: {
           primary: "#0097a9",
-          secondary: "#ffffff",
+          secondary: "#F2A900",
+          accent: "#244C5A",
           anchor: "#00818f",
+          info: "#0097a9",
+          success: "#7A9A01",
+          warning: "#F2A900",
+          error: "#DC4405",
         },
       },
     },
@@ -27,26 +32,3 @@ export default createVuetify({
     },
   },
 })
-
-/* --blue:#007bff;
---indigo:#6610f2;
---purple:#6f42c1;
---pink:#e83e8c;
---red:#dc3545;
---orange:#fd7e14;
---yellow:#ffc107;
---green:#28a745;
---teal:#20c997;
---cyan:#17a2b8;
---white:#fff;
---gray:#6c757d;
---gray-dark:#343a40;
---primary:#007bff;
---secondary:#6c757d;
---success:#28a745;
---info:#17a2b8;
---warning:#ffc107;
---danger:#dc3545;
---light:#f8f9fa;
---dark:#343a40;
- */
