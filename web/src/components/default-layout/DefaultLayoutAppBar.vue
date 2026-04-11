93c1604322942d830d3035496fdc5c761a6ed328
Marlen Brunner <klondikemarlen@gmail.com>
2026-04-11 11:56:56 -0700

    :recycle: Extract default layout app bar.

---
+++ web/src/components/default-layout/DefaultLayoutAppBar.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app-bar
+    app
+    color="#fff"
+    elevation="0"
+    height="70"
+    style="border-bottom: 3px #f3b228 solid"
+  >
+    <v-app-bar-nav-icon
+      title="Show Navigation"
+      @click="emit('toggleDrawer')"
+    />
+    <v-btn
+      :to="{
+        name: 'DashboardPage',
+      }"
+      class="pa-0 no-change-on-hover"
+      height="44"
+      style="margin-top: -8px"
+      variant="text"
+    >
+      <img
+        src="/yukon.svg"
+        height="44"
+      />
+    </v-btn>
+    <v-toolbar-title class="ml-16 d-none d-md-block">
+      <h1 class="text-h6 font-weight-bold mb-0">{{ APPLICATION_NAME }}</h1>
+    </v-toolbar-title>
+
+    <v-spacer />
+
+    <v-btn
+      icon="mdi-history"
+      variant="text"
+      color="primary"
+      class="mr-2"
+      title="Recently visited"
+      @click="showHistory"
+    />
+
+    <v-btn
+      variant="text"
+      :to="{
+        name: 'ProfilePage',
+      }"
+    >
+      {{ fullName }}
+    </v-btn>
+
+    <KebabMenu />
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+import { APPLICATION_NAME } from "@/config"
+
+import useCurrentUser from "@/use/use-current-user"
+
+import KebabMenu from "@/components/default-layout/KebabMenu.vue"
+
+const emit = defineEmits<{
+  toggleDrawer: [void]
+}>()
+
+const { fullName } = useCurrentUser()
+
+function showHistory() {
+  alert("TODO: implement history")
+}
+</script>
+
+<style scoped>
+.no-change-on-hover {
+  background-color: transparent !important;
+  color: white !important;
+}
+
+.no-change-on-hover:hover {
+  background-color: transparent !important;
+  color: white !important;
+}
+</style>
--- web/src/layouts/DefaultLayout.vue
+++ web/src/layouts/DefaultLayout.vue
@@ -2,55 +2,7 @@
   <v-layout>
     <LeftSidebarNavigationDrawer ref="leftSidebarNavigationDrawerRef" />
 
-    <v-app-bar
-      app
-      color="#fff"
-      elevation="0"
-      height="70"
-      style="border-bottom: 3px #f3b228 solid"
-    >
-      <v-app-bar-nav-icon
-        title="Show Navigation"
-        @click="toggleDrawer"
-      />
-      <v-btn
-        :to="{ name: 'DashboardPage' }"
-        class="pa-0 no-change-on-hover"
-        height="44"
-        style="margin-top: -8px"
-        variant="text"
-      >
-        <img
-          src="/yukon.svg"
-          height="44"
-        />
-      </v-btn>
-      <v-toolbar-title class="ml-16 d-none d-md-block">
-        <h1 class="text-h6 font-weight-bold mb-0">{{ APPLICATION_NAME }}</h1>
-      </v-toolbar-title>
-
-      <v-spacer />
-
-      <v-btn
-        icon="mdi-history"
-        variant="text"
-        color="primary"
-        class="mr-2"
-        title="Recently visited"
-        @click="showHistory"
-      />
-
-      <v-btn
-        variant="text"
-        :to="{
-          name: 'ProfilePage',
-        }"
-      >
-        {{ fullName }}
-      </v-btn>
-
-      <KebabMenu />
-    </v-app-bar>
+    <DefaultLayoutAppBar @toggle-drawer="toggleDrawer" />
 
     <v-main class="grey lighten-4">
       <!-- Provides the application the proper gutter -->
@@ -67,43 +19,20 @@
 
 <script setup lang="ts">
 import { useTemplateRef } from "vue"
-import { useDisplay } from "vuetify"
 
-import { APPLICATION_NAME } from "@/config"
-
-import useCurrentUser from "@/use/use-current-user"
-
-import KebabMenu from "@/components/default-layout/KebabMenu.vue"
+import DefaultLayoutAppBar from "@/components/default-layout/DefaultLayoutAppBar.vue"
 import UnsetDepartmentAlert from "@/components/default-layout/UnsetDepartmentAlert.vue"
 import LeftSidebarNavigationDrawer from "@/components/default-layout/LeftSidebarNavigationDrawer.vue"
-
-const { fullName } = useCurrentUser()
-
-useDisplay()
 
 const leftSidebarNavigationDrawerRef = useTemplateRef("leftSidebarNavigationDrawerRef")
 
 function toggleDrawer() {
   leftSidebarNavigationDrawerRef.value?.toggle()
 }
-
-function showHistory() {
-  alert("TODO: implement history")
-}
 </script>
 
 <style scoped>
 .h-full {
   height: 100%;
-}
-
-.no-change-on-hover {
-  background-color: transparent !important;
-  color: white !important;
-}
-
-.no-change-on-hover:hover {
-  background-color: transparent !important;
-  color: white !important;
 }
 </style>
