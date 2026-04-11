96a7a9778fede5fd5c2e712f4e0fedab4eb4844b
Marlen Brunner <klondikemarlen@gmail.com>
2026-04-10 18:24:16 -0700

    :cherry_blossom: Increase refresh icon size without enlarging button hit area.
    Keep button small, but increase size of icon manually for prettier display.

--- web/src/components/common/table/RefreshTableButton.vue
+++ web/src/components/common/table/RefreshTableButton.vue
@@ -1,12 +1,14 @@
 <template>
   <v-btn
     class="ma-0"
-    icon="mdi-cached"
+    icon
     size="small"
     variant="text"
     color="primary"
     title="Refresh Table"
-  />
+  >
+    <v-icon size="28">mdi-cached</v-icon>
+  </v-btn>
 </template>
 
 <script setup></script>
