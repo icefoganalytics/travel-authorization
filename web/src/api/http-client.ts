26823d81664ff916556d9533ab8dada3f050f335
Marlen Brunner <klondikemarlen@gmail.com>
2026-04-13 09:30:41 -0700

    :recycle: Convert http-client to Typescript and align with Wrap project patterns.

--- web/src/api/http-client.ts
+++ web/src/api/http-client.ts
@@ -1,8 +1,9 @@
-import axios from "axios"
 import qs from "qs"
+import axios from "axios"
 
-import auth0 from "@/plugins/auth0-plugin"
 import { API_BASE_URL } from "@/config"
+import auth0 from "@/plugins/auth0-plugin"
+import ApiError from "@/api/api-error"
 
 export const httpClient = axios.create({
   baseURL: API_BASE_URL,
@@ -14,30 +15,34 @@ export const httpClient = axios.create({
       return qs.stringify(params, {
         arrayFormat: "indices",
         strictNullHandling: true,
+        allowEmptyArrays: true,
       })
     },
   },
 })
 
 httpClient.interceptors.request.use(async (config) => {
-  const accessToken = await auth0.getAccessTokenSilently()
-  config.headers["Authorization"] = `Bearer ${accessToken}`
+  // Only add the Authorization header to requests that start with "/api"
+  if (config.url?.startsWith("/api")) {
+    const accessToken = await auth0.getAccessTokenSilently()
+    config.headers["Authorization"] = `Bearer ${accessToken}`
+  }
+
+  config.headers["X-Full-Referrer"] = window.location.href
+
   return config
 })
 
 // Any status codes that falls outside the range of 2xx causes this function to trigger
 httpClient.interceptors.response.use(null, async (error) => {
-  // Auth0 error type is unknown but it sets the error.error property to "login_required"
-  // Bounce the user if they hit a login required error when trying to access a protected route
-  // It would probably be better to move this code to a route guard or something?
   if (error?.error === "login_required") {
-    throw new Error("You must be logged in to access this endpoint")
+    throw new ApiError("You must be logged in to access this endpoint", 401)
   } else if (error?.response?.data?.message) {
-    throw new Error(error.response.data.message)
+    throw new ApiError(error.response.data.message, error.response.status)
   } else if (error.message) {
-    throw new Error(error.message)
+    throw new ApiError(error.message, error.response?.status || 500)
   } else {
-    throw new Error("An unknown error occurred")
+    throw new ApiError("An unknown error occurred", error.response?.status || 500)
   }
 })
 
