378347235c0f125309121ceb01380da5364c9a25
Marlen Brunner <klondikemarlen@gmail.com>
2026-05-27 08:23:54 -0700

    :recycle: Add ShowSerializer for TravelAuthorizationPreApprovals and convert composable to TypeScript

--- api/src/serializers/travel-authorization-pre-approvals/index.ts
+++ api/src/serializers/travel-authorization-pre-approvals/index.ts
@@ -3,6 +3,10 @@ export {
   type TravelAuthorizationPreApprovalAsIndex as AsIndex,
 } from "./index-serializer"
 export {
+  ShowSerializer,
+  type TravelAuthorizationPreApprovalAsShow as AsShow,
+} from "./show-serializer"
+export {
   ReferenceSerializer,
   type TravelAuthorizationPreApprovalAsReference as AsReference,
 } from "./reference-serializer"
