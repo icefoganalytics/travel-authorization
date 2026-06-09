c1f2fc496cbadd4738852c68b826bf9d4e63117e
Marlen Brunner <klondikemarlen@gmail.com>
2026-06-09 14:53:32 -0700

    :cherry_blossom: Align TravelRequestLayout tab order with other layouts.
    Moves SummaryHeaderPanel below tabs.

--- web/src/layouts/TravelRequestLayout.vue
+++ web/src/layouts/TravelRequestLayout.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <SummaryHeaderPanel
-      ref="summaryHeaderPanel"
-      :travel-authorization-id="travelAuthorizationIdAsNumber"
-    />
-
     <v-tabs>
       <v-tab
         :to="{
@@ -35,6 +30,12 @@
       </v-tab>
       <!-- TODO: add in any tabs that you can normally see in manage mode -->
     </v-tabs>
+
+    <SummaryHeaderPanel
+      ref="summaryHeaderPanel"
+      :travel-authorization-id="travelAuthorizationIdAsNumber"
+      class="mt-4"
+    />
 
     <router-view v-slot="{ Component }">
       <component
